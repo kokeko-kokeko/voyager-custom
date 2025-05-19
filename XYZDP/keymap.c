9d40b07fb7c3768e3b55e1efbc449c5701061623
kokeko-kokeko <kokeko@mac.com>
2025-05-19 23:14:35 +0900

    fix LED

--- XYZDP/keymap.c
+++ XYZDP/keymap.c
@@ -1099,15 +1099,15 @@ layer_state_t layer_state_set_user(layer_state_t state) {
       STATUS_LED_4(1);
       break;
     case 14:
-      STATUS_LED_1(1);
-      STATUS_LED_2(1);
-      STATUS_LED_3(0);
-      STATUS_LED_4(1);
-      break;
-    case 15:
       STATUS_LED_1(0);
       STATUS_LED_2(1);
       STATUS_LED_3(1);
+      STATUS_LED_4(1);
+      break;
+    case 15:
+      STATUS_LED_1(1);
+      STATUS_LED_2(1);
+      STATUS_LED_3(0);
       STATUS_LED_4(1);
       break;
     default:
