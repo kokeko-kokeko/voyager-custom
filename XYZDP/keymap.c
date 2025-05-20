112b2f21e4d5573db613dc293130818c687748b1
kokeko-kokeko <kokeko@mac.com>
2025-05-20 12:26:24 +0900

    Update Tap Flow

--- XYZDP/keymap.c
+++ XYZDP/keymap.c
@@ -1132,3 +1132,12 @@ bool is_flow_tap_key(uint16_t keycode) {
   }
   return false;
 }
+// Term calc
+uint16_t get_flow_tap_term(uint16_t keycode, keyrecord_t* record,
+                           uint16_t prev_keycode) {
+  if (is_flow_tap_key(keycode) && is_flow_tap_key(prev_keycode)) {
+    return FLOW_TAP_TERM;
+  }
+  return 0;
+}
+
