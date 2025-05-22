81227fb3756fa2df288c04daf95e3b7717f46288
kokeko-kokeko <kokeko@mac.com>
2025-05-22 16:30:02 +0900

    Update Tap Flow control

--- XYZDP/keymap.c
+++ XYZDP/keymap.c
@@ -1291,8 +1291,11 @@ tap_dance_action_t tap_dance_actions[] = {
 //
 // additional
 //
-// tap flow cntrol thumb space LT 
-// and pinkey outer col
+// tap flow cntrol
+// is_flow_tap_key is default
+// disable (return 0)
+// thumb space LT 
+// pinkey outer col
 uint16_t get_flow_tap_term(uint16_t keycode, keyrecord_t* record, 
                            uint16_t prev_keycode) {
   if (is_flow_tap_key(keycode) && is_flow_tap_key(prev_keycode)) {
