61ef1e98a68127a6ab014a20fe81efb7efa2b85c
kokeko-kokeko <kokeko@mac.com>
2025-05-26 15:32:02 +0900

    Update keymap.c
    update LED animation

--- XYZDP/keymap.c
+++ XYZDP/keymap.c
@@ -1345,7 +1345,7 @@ uint16_t get_flow_tap_term(uint16_t keycode, keyrecord_t* record,
 // other: output value & wait
 static const uint16_t * const led_pattern_blink = (uint16_t[]){10, 300, 190, UINT16_MAX, UINT16_MAX, UINT16_MAX};
 static const uint16_t * const led_pattern_off = (uint16_t[]){0, UINT16_MAX, UINT16_MAX, UINT16_MAX};
-static const uint16_t * const led_pattern_on = (uint16_t[]){10, 0, UINT16_MAX, UINT16_MAX, UINT16_MAX};
+//static const uint16_t * const led_pattern_on = (uint16_t[]){10, 0, UINT16_MAX, UINT16_MAX, UINT16_MAX};
 static const uint16_t * const led_pattern_oneshot = (uint16_t[]){10, 500, 100, 400, 100, 300, 100, 200, 100, 100, 0,UINT16_MAX, UINT16_MAX, UINT16_MAX};
 
 static uint32_t led_pattern_task_1(uint32_t trigger_time, void *cb_arg) {
@@ -1501,7 +1501,10 @@ layer_state_t layer_state_set_user(layer_state_t state) {
       led_pattern(0b1100, led_pattern_off, 0);
       break;
     default:
-      led_pattern(0b1111, led_pattern_on, 0);
+      led_pattern(0b1000, led_pattern_blink, 0);
+      led_pattern(0b0100, led_pattern_blink, 100);
+      led_pattern(0b0010, led_pattern_blink, 200);
+      led_pattern(0b0001, led_pattern_blink, 300);
       break;
   }
   return state;
