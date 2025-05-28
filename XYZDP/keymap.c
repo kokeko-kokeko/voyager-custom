4924828aa1ae7b4dfb7e6a607ddd584701a28bdc
kokeko-kokeko <kokeko@mac.com>
2025-05-28 13:02:07 +0900

    Update keymap.c
    off is NULL

--- XYZDP/keymap.c
+++ XYZDP/keymap.c
@@ -1332,7 +1332,7 @@ uint16_t get_flow_tap_term(uint16_t keycode, keyrecord_t* record,
 // other: output current position value & wait
 // put reurn token for safety
 // max 16x255=4080ms 4sec
-static const uint8_t * const led_pattern_off = (uint8_t[]){0, UINT8_MAX, UINT8_MAX, UINT8_MAX};
+static const uint8_t * const led_pattern_off = NULL;
 static const uint8_t * const led_pattern_on = (uint8_t[]){1, 0, UINT8_MAX, UINT8_MAX, UINT8_MAX};
 static const uint8_t * const led_pattern_blink = (uint8_t[]){16, 31, UINT8_MAX, UINT8_MAX, UINT8_MAX};
 static const uint8_t * const led_pattern_oneshot = (uint8_t[]){1, 24, 32, 24, 32, 24, 32, 24, 32, 24, 32, 24, 32, 24, 32, 24, 0, UINT8_MAX, UINT8_MAX, UINT8_MAX};
