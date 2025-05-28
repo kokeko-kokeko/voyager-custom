c0de16d0b24c0c0d30fdf99695ea203408d26594
kokeko-kokeko <kokeko@mac.com>
2025-05-28 15:28:29 +0900

    Update keymap.c
    update LED animation

--- XYZDP/keymap.c
+++ XYZDP/keymap.c
@@ -1334,8 +1334,8 @@ uint16_t get_flow_tap_term(uint16_t keycode, keyrecord_t* record,
 // max 16x255=4080ms 4sec
 static const uint8_t * const led_pattern_off = NULL;
 static const uint8_t * const led_pattern_on = (uint8_t[]){1, 0, UINT8_MAX, UINT8_MAX, UINT8_MAX};
-static const uint8_t * const led_pattern_blink = (uint8_t[]){22, 41, UINT8_MAX, UINT8_MAX, UINT8_MAX};
-static const uint8_t * const led_pattern_oneshot = (uint8_t[]){16, 24, 6, 24, 6, 24, 6, 24, 6, 24, 6, 24, 6, 24, 6, 24, 0, UINT8_MAX, UINT8_MAX, UINT8_MAX};
+static const uint8_t * const led_pattern_blink = (uint8_t[]){13, 50, UINT8_MAX, UINT8_MAX, UINT8_MAX};
+static const uint8_t * const led_pattern_oneshot = (uint8_t[]){13, 20, 6, 20, 6, 20, 6, 20, 6, 20, 6, 20, 6, 20, 6, 20, 0, UINT8_MAX, UINT8_MAX, UINT8_MAX};
 
 static uint32_t status_led_task_1(uint32_t trigger_time, void *cb_arg) {
   static uint8_t count = 0;
