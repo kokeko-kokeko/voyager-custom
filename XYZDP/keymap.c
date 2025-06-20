565baf69586450bb6aa5a6a57c0193b3ab86e781
kokeko-kokeko <kokeko@mac.com>
2025-06-21 01:09:01 +0900

    Update keymap.c
    fn key override

--- XYZDP/keymap.c
+++ XYZDP/keymap.c
@@ -2226,11 +2226,14 @@ const key_override_t ko_jis_base_COMMs = ko_make_with_layers(MOD_MASK_SHIFT, KC_
 const key_override_t ko_jis_base_DOTs  = ko_make_with_layers(MOD_MASK_SHIFT, KC_DOT , KC_QUOT, 1 << 1);
 const key_override_t ko_jis_base_SLSHs = ko_make_with_layers(MOD_MASK_SHIFT, KC_SLSH, KC_INT1, 1 << 1);
 
+const key_override_t ko_jis_fn_a = ko_make_with_layers(0, KC_A, LCTL(KC_RBRC), 1 << 6);
+
 const key_override_t **key_overrides_jis =
 (const key_override_t *[]){ &ko_jis_base_0,  &ko_jis_base_1,  &ko_jis_base_2,  &ko_jis_base_3,  &ko_jis_base_4, 
                             &ko_jis_base_0s, &ko_jis_base_1s, &ko_jis_base_2s, &ko_jis_base_3s, &ko_jis_base_4s,
                             &ko_jis_base_QUOTs, &ko_jis_base_COMMs, &ko_jis_base_DOTs, &ko_jis_base_SLSHs,
                             &ko_com_LANG1s, &ko_com_LANG2s,
+                            &ko_jis_fn_a,
                             NULL, NULL, NULL};
 
 // declaration for qmk function
