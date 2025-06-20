64b6e918aef02b22fa9cd6d4e3d3b2919864d8a9
kokeko-kokeko <kokeko@mac.com>
2025-06-20 12:05:05 +0900

    Update keymap.c
    update override

--- XYZDP/keymap.c
+++ XYZDP/keymap.c
@@ -1915,26 +1915,31 @@ const key_override_t ko_jis_base_DOTs = ko_make_with_layers(MOD_MASK_SHIFT, KC_D
 const key_override_t ko_jis_base_SLSHs = ko_make_with_layers(MOD_MASK_SHIFT, KC_SLSH, JP_BSLS, 1 << 0);
 
 // ANSI to JIS
-const key_override_t ko_ansi_to_jis_0  = ko_make_basic(MOD_MASK_SHIFT, KC_GRV, JP_TILD);
-const key_override_t ko_ansi_to_jis_1  = ko_make_basic(             0, KC_GRV, JP_GRV);
-const key_override_t ko_ansi_to_jis_2  = ko_make_basic(MOD_MASK_SHIFT, KC_2, JP_AT);
-const key_override_t ko_ansi_to_jis_3  = ko_make_basic(MOD_MASK_SHIFT, KC_6, JP_CIRC);
-const key_override_t ko_ansi_to_jis_4  = ko_make_basic(MOD_MASK_SHIFT, KC_7, JP_AMPR);
-const key_override_t ko_ansi_to_jis_5  = ko_make_basic(MOD_MASK_SHIFT, KC_8, JP_ASTR);
-const key_override_t ko_ansi_to_jis_6  = ko_make_basic(MOD_MASK_SHIFT, KC_9, JP_LPRN);
-const key_override_t ko_ansi_to_jis_7  = ko_make_basic(MOD_MASK_SHIFT, KC_0, JP_RPRN);
-const key_override_t ko_ansi_to_jis_8  = ko_make_basic(MOD_MASK_SHIFT, KC_MINS, JP_UNDS);
-const key_override_t ko_ansi_to_jis_9  = ko_make_basic(             0, KC_MINS, JP_MINS);
-const key_override_t ko_ansi_to_jis_10 = ko_make_basic(MOD_MASK_SHIFT, KC_EQL, JP_PLUS);
-const key_override_t ko_ansi_to_jis_11 = ko_make_basic(             0, KC_EQL, JP_EQL);
-const key_override_t ko_ansi_to_jis_12 = ko_make_basic(             0, KC_LBRC , JP_LBRC);
-const key_override_t ko_ansi_to_jis_13 = ko_make_basic(             0, KC_RBRC , JP_RBRC);
-const key_override_t ko_ansi_to_jis_14 = ko_make_basic(MOD_MASK_SHIFT, KC_BSLS , JP_PIPE);
-const key_override_t ko_ansi_to_jis_15 = ko_make_basic(             0, KC_BSLS , JP_BSLS);
-const key_override_t ko_ansi_to_jis_16 = ko_make_basic(MOD_MASK_SHIFT, KC_SCLN , JP_COLN);
-const key_override_t ko_ansi_to_jis_17 = ko_make_basic(             0, KC_SCLN , JP_SCLN);
-const key_override_t ko_ansi_to_jis_18 = ko_make_basic(MOD_MASK_SHIFT, KC_QUOT , JP_DQUO);
-const key_override_t ko_ansi_to_jis_19 = ko_make_basic(             0, KC_QUOT , JP_QUOT);
+const key_override_t ko_ansi_to_jis_0  = ko_make_with_layers_and_negmods(0, KC_GRV, JP_GRV, ~0, (uint8_t) MOD_MASK_SHIFT);
+const key_override_t ko_ansi_to_jis_1  = ko_make_with_layers_and_negmods(0, KC_MINS, JP_MINS, ~0, (uint8_t) MOD_MASK_SHIFT);
+const key_override_t ko_ansi_to_jis_2  = ko_make_with_layers_and_negmods(0, KC_EQL, JP_EQL, ~0, (uint8_t) MOD_MASK_SHIFT);
+const key_override_t ko_ansi_to_jis_3  = ko_make_with_layers_and_negmods(0, KC_LBRC , JP_LBRC, ~0, (uint8_t) MOD_MASK_SHIFT);
+const key_override_t ko_ansi_to_jis_4  = ko_make_with_layers_and_negmods(0, KC_RBRC , JP_RBRC, ~0, (uint8_t) MOD_MASK_SHIFT);
+const key_override_t ko_ansi_to_jis_5  = ko_make_with_layers_and_negmods(0, KC_BSLS , JP_BSLS, ~0, (uint8_t) MOD_MASK_SHIFT);
+const key_override_t ko_ansi_to_jis_6  = ko_make_with_layers_and_negmods(0, KC_SCLN , JP_SCLN, ~0, (uint8_t) MOD_MASK_SHIFT);
+const key_override_t ko_ansi_to_jis_7  = ko_make_with_layers_and_negmods(0, KC_QUOT , JP_QUOT, ~0, (uint8_t) MOD_MASK_SHIFT);
+
+const key_override_t ko_ansi_to_jis_8  = ko_make_basic(MOD_MASK_SHIFT, KC_GRV, JP_TILD);
+const key_override_t ko_ansi_to_jis_9  = ko_make_basic(MOD_MASK_SHIFT, KC_2, JP_AT);
+const key_override_t ko_ansi_to_jis_10 = ko_make_basic(MOD_MASK_SHIFT, KC_6, JP_CIRC);
+const key_override_t ko_ansi_to_jis_11 = ko_make_basic(MOD_MASK_SHIFT, KC_7, JP_AMPR);
+const key_override_t ko_ansi_to_jis_12 = ko_make_basic(MOD_MASK_SHIFT, KC_8, JP_ASTR);
+const key_override_t ko_ansi_to_jis_13 = ko_make_basic(MOD_MASK_SHIFT, KC_9, JP_LPRN);
+const key_override_t ko_ansi_to_jis_14 = ko_make_basic(MOD_MASK_SHIFT, KC_0, JP_RPRN);
+const key_override_t ko_ansi_to_jis_15 = ko_make_basic(MOD_MASK_SHIFT, KC_MINS, JP_UNDS);
+const key_override_t ko_ansi_to_jis_16 = ko_make_basic(MOD_MASK_SHIFT, KC_EQL, JP_PLUS);
+
+const key_override_t ko_ansi_to_jis_17 = ko_make_basic(MOD_MASK_SHIFT, KC_LBRC, JP_LCBR);
+const key_override_t ko_ansi_to_jis_18 = ko_make_basic(MOD_MASK_SHIFT, KC_RBRC, JP_RCBR);
+const key_override_t ko_ansi_to_jis_19 = ko_make_basic(MOD_MASK_SHIFT, KC_BSLS , JP_PIPE);
+
+const key_override_t ko_ansi_to_jis_20 = ko_make_basic(MOD_MASK_SHIFT, KC_SCLN , JP_COLN);
+const key_override_t ko_ansi_to_jis_21 = ko_make_basic(MOD_MASK_SHIFT, KC_QUOT , JP_DQUO);
 
 const key_override_t **key_overrides_jis =
 (const key_override_t *[]){ &ko_jis_base_1, &ko_jis_base_2, &ko_jis_base_3, &ko_jis_base_4, &ko_jis_base_5, 
@@ -1943,7 +1948,8 @@ const key_override_t **key_overrides_jis =
                             &ko_ansi_to_jis_0, &ko_ansi_to_jis_1, &ko_ansi_to_jis_2, &ko_ansi_to_jis_3, &ko_ansi_to_jis_4, 
                             &ko_ansi_to_jis_5, &ko_ansi_to_jis_6, &ko_ansi_to_jis_7, &ko_ansi_to_jis_8, &ko_ansi_to_jis_9, 
                             &ko_ansi_to_jis_10, &ko_ansi_to_jis_11, &ko_ansi_to_jis_12, &ko_ansi_to_jis_13, &ko_ansi_to_jis_14, 
-                            &ko_ansi_to_jis_15, &ko_ansi_to_jis_16, &ko_ansi_to_jis_17, &ko_ansi_to_jis_18, &ko_ansi_to_jis_19,  
+                            &ko_ansi_to_jis_15, &ko_ansi_to_jis_16, &ko_ansi_to_jis_17, &ko_ansi_to_jis_18, &ko_ansi_to_jis_19,
+                            &ko_ansi_to_jis_20, &ko_ansi_to_jis_21,
                             NULL, NULL, NULL};
 
 // declaration for qmk function
