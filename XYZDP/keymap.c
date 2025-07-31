9d8b50a623ca302e73aed589a7501c4693cbed10
kokeko-kokeko <kokeko@mac.com>
2025-07-31 15:21:24 +0900

    Update keymap.c
    refactor

--- XYZDP/keymap.c
+++ XYZDP/keymap.c
@@ -2289,9 +2289,8 @@ static void set_layer_color_overlay(void) {
   HSV hsv;
 
   // copy logic from breathing_anim.h 
-  uint8_t speed = rgb_matrix_get_speed();
-
- // more fast speed (8/5)
+  // more fast speed (8/5)
+  uint8_t speed = rgb_matrix_get_speed(); 
   uint16_t time = 0;
   uint8_t b_val = 0;
   
