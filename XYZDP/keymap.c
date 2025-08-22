f5f8488d9597ce81f9edb2abf4507df42f7755c4
kokeko-kokeko <kokeko@mac.com>
2025-08-23 00:47:51 +0900

    Update keymap.c

--- XYZDP/keymap.c
+++ XYZDP/keymap.c
@@ -419,30 +419,7 @@ extern keyboard_config_t keyboard_config;
 extern bool is_launching;
 
 // enum for layer define sync oryx side
-enum layer_num {
-  L_Base = 0,
-  L_Base_JIS,
-  L_MO_Guard,
-  L_Function,
-  L_Number,
-  L_Number_JIS,
-  L_Cursor,
-  L_Cursor_JIS,
-  L_LeftPinky,
-  L_RightPinky,
-  L_LeftPinkyThumb,
-  L_RightPinkyThumb,
-  L_BothThumb,
-  L_BothThumb_JIS,
-  L_BothPinky,
-  L_BothPinkyThumb,
-  L_Firmware,
-  L_Set_Speed,
-  L_Set_Val,
-  L_Set_Sat,
-  L_Set_Hue,
-  C_LAYER_COUNT 
-};
+
 
 #define ORYX_LAYER_COUNT (sizeof(keymaps) / sizeof(keymaps[0]))
 _Static_assert(C_LAYER_COUNT == ORYX_LAYER_COUNT, "C and ORYX layer count missmatch!!");
