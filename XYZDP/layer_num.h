2aefe1510058a8d66d5c7e712d231c6f9d03db0f
kokeko-kokeko <kokeko@mac.com>
2025-08-23 00:54:12 +0900

    Create layer_num.h

---
+++ XYZDP/layer_num.h
@@ -0,0 +1,26 @@
+// layer 
+enum layer_num {
+  L_Base = 0,
+  L_Base_JIS,
+  L_MO_Guard,
+  L_Function,
+  L_Number,
+  L_Number_JIS,
+  L_Cursor,
+  L_Cursor_JIS,
+  L_LeftPinky,
+  L_RightPinky,
+  L_LeftPinkyThumb,
+  L_RightPinkyThumb,
+  L_BothThumb,
+  L_BothThumb_JIS,
+  L_BothPinky,
+  L_BothPinkyThumb,
+  L_Firmware,
+  L_Set_Speed,
+  L_Set_Val,
+  L_Set_Sat,
+  L_Set_Hue,
+  C_LAYER_COUNT 
+};
+
