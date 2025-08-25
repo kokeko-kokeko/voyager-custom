44bb6d2c014715d45ec66b2bb1c811b608547de5
kokeko-kokeko <kokeko@mac.com>
2025-08-25 14:29:18 +0900

    Update fade_matrix.h

--- XYZDP/fade_matrix.h
+++ XYZDP/fade_matrix.h
@@ -1,1 +1,17 @@
+#pragma once
 
+extern void fade_matrix_load_preset(void);
+extern void fade_matrix_load_preset_powersave(void);
+extern void fade_matrix_set_hue_pos(unit8_t pos);
+extern void fade_matrix_set_sat_pos(unit8_t pos);
+extern void fade_matrix_set_val_pos(unit8_t pos);
+extern void fade_matrix_set_speed_pos(unit8_t pos);
+
+extern void init_fade_matrix(const fast_timer_t now);
+extern void update_fade_matrix(const fast_timer_t now);
+
+extern void set_layer_color_firmware_map(void);
+extern void set_layer_color_hue_map(void);
+extern void set_layer_color_sat_map(void);
+extern void set_layer_color_val_map(void);
+extern void set_layer_color_speed_map(void);
