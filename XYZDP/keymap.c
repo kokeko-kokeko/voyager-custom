1d6a40e539a72147cc6b98f5207981f4470d4448
kokeko-kokeko <kokeko@mac.com>
2025-08-27 17:40:53 +0900

    Update keymap.c

--- XYZDP/keymap.c
+++ XYZDP/keymap.c
@@ -558,8 +558,8 @@ bool rgb_matrix_indicators_user(void) {
         rgb_matrix_set_color_all(0, 0, 0);
       } else {
         set_layer_color_overlay_mod();
-        set_layer_color_overlay_layer();
         set_layer_color_overlay_base();
+        set_layer_color_overlay_layer();
       }
       break;
   }
