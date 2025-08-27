566640cd3e5a3c5fbc33e4d856a9f1bff71d1e69
kokeko-kokeko <kokeko@mac.com>
2025-08-27 17:32:37 +0900

    Update keymap.c

--- XYZDP/keymap.c
+++ XYZDP/keymap.c
@@ -435,6 +435,7 @@ _Static_assert(C_LAYER_COUNT == ORYX_LAYER_COUNT, "C and ORYX layer count missma
 extern keyboard_config_t keyboard_config;
 extern bool is_launching;
 
+// split impl
 #include "fade_matrix.h"
 #include "overlay_layer_mod.h"
 #include "status_led.h"
@@ -2375,72 +2376,6 @@ static void set_layer_color_overlay_base(void) {
   uint16_t time = scale16by8(g_rgb_timer, speed / 3);
   hsv.v = scale8(abs8(sin8(time) - 128) * 2, hsv.v);
 
-  // CAPS WORD inidication
-  
-  if (is_caps_word_on()) {
-    hsv.h -= h_diff;
-    RGB rgb = hsv_to_rgb(hsv);
-    rgb_matrix_set_color(0, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(31, rgb.r, rgb.g, rgb.b);
-  }
-
-  // mods display
-  if (get_mods() & MOD_BIT_LCTRL) {
-    hsv.h -= h_diff;
-    RGB rgb = hsv_to_rgb(hsv);
-    rgb_matrix_set_color(4, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(10, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(12, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(13, rgb.r, rgb.g, rgb.b);
-  }
-  if (get_mods() & MOD_BIT_LSHIFT) {
-    hsv.h -= h_diff;
-    RGB rgb = hsv_to_rgb(hsv);
-    rgb_matrix_set_color(3, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(9, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(14, rgb.r, rgb.g, rgb.b);
-  }
-  if (get_mods() & MOD_BIT_LALT) {
-    hsv.h -= h_diff;
-    RGB rgb = hsv_to_rgb(hsv);
-    rgb_matrix_set_color(2, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(8, rgb.r, rgb.g, rgb.b);
-  }
-  if (get_mods() & MOD_BIT_LGUI) {
-    hsv.h -= h_diff;
-    RGB rgb = hsv_to_rgb(hsv);
-    rgb_matrix_set_color(1, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(7, rgb.r, rgb.g, rgb.b);
-  }
-  
-  if (get_mods() & MOD_BIT_RCTRL) {
-    hsv.h -= h_diff;
-    RGB rgb = hsv_to_rgb(hsv);
-    rgb_matrix_set_color(27, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(33, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(42, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(43, rgb.r, rgb.g, rgb.b);
-  }
-  if (get_mods() & MOD_BIT_RSHIFT) {
-    hsv.h -= h_diff;
-    RGB rgb = hsv_to_rgb(hsv);
-    rgb_matrix_set_color(28, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(34, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(41, rgb.r, rgb.g, rgb.b);
-  }
-  if (get_mods() & MOD_BIT_RALT) {
-    hsv.h -= h_diff;
-    RGB rgb = hsv_to_rgb(hsv);
-    rgb_matrix_set_color(29, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(35, rgb.r, rgb.g, rgb.b);
-  }
-  if (get_mods() & MOD_BIT_RGUI) {
-    hsv.h -= h_diff;
-    RGB rgb = hsv_to_rgb(hsv);
-    rgb_matrix_set_color(30, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(36, rgb.r, rgb.g, rgb.b);
-  }
-
   // IME state sync syntem state
   if (ime_on) {
     hsv.h -= h_diff;
@@ -2463,101 +2398,6 @@ static void set_layer_color_overlay_base(void) {
     rgb_matrix_set_color(17, 0, 0, 0);
     rgb_matrix_set_color(26, 0, 0, 0);
     rgb_matrix_set_color(32, 0, 0, 0);
-  }
-  
-  // layer display with priority
-  if (layer_state_is(L_BothPinkyThumb)) {
-    hsv.h -= h_diff;
-    RGB rgb = hsv_to_rgb(hsv);
-    rgb_matrix_set_color(5, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(6, rgb.r, rgb.g, rgb.b);
-
-    rgb_matrix_set_color(26, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(37, rgb.r, rgb.g, rgb.b);
-
-    rgb_matrix_set_color(11, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(22, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(50, rgb.r, rgb.g, rgb.b);
-
-    rgb_matrix_set_color(25, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(32, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(45, rgb.r, rgb.g, rgb.b);
-    return;
-  }
-  if (layer_state_is(L_BothPinky)) {
-    hsv.h -= h_diff;
-    RGB rgb = hsv_to_rgb(hsv);
-    rgb_matrix_set_color(5, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(6, rgb.r, rgb.g, rgb.b);
-
-    rgb_matrix_set_color(26, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(37, rgb.r, rgb.g, rgb.b);
-    return;
-  }
-  if (layer_state_is(L_BothThumb)) {
-    hsv.h -= h_diff;
-    RGB rgb = hsv_to_rgb(hsv);
-    rgb_matrix_set_color(11, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(22, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(50, rgb.r, rgb.g, rgb.b);
-
-    rgb_matrix_set_color(25, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(32, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(45, rgb.r, rgb.g, rgb.b);
-    return;
-  }
-  if (layer_state_is(L_RightPinkyThumb)) {
-    hsv.h -= h_diff;
-    RGB rgb = hsv_to_rgb(hsv);
-    rgb_matrix_set_color(26, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(37, rgb.r, rgb.g, rgb.b);
-    
-    rgb_matrix_set_color(25, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(32, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(45, rgb.r, rgb.g, rgb.b);
-
-    return;
-  }
-  if (layer_state_is(L_LeftPinkyThumb)) {
-    hsv.h -= h_diff;
-    RGB rgb = hsv_to_rgb(hsv);
-    rgb_matrix_set_color(5, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(6, rgb.r, rgb.g, rgb.b);
-
-    rgb_matrix_set_color(11, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(22, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(50, rgb.r, rgb.g, rgb.b);
-    return;
-  }
-  if (layer_state_is(L_RightPinky)) {
-    hsv.h -= h_diff;
-    RGB rgb = hsv_to_rgb(hsv);
-    rgb_matrix_set_color(26, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(37, rgb.r, rgb.g, rgb.b);
-    return;
-  }
-  if (layer_state_is(L_LeftPinky)) {
-    hsv.h -= h_diff;
-    RGB rgb = hsv_to_rgb(hsv);
-    rgb_matrix_set_color(5, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(6, rgb.r, rgb.g, rgb.b);
-    return;
-  }
-  if (layer_state_is(L_Cursor)) {
-    hsv.h -= h_diff;
-    RGB rgb = hsv_to_rgb(hsv);
-    rgb_matrix_set_color(25, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(32, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(45, rgb.r, rgb.g, rgb.b);
-    return;
-  }
-  if (layer_state_is(L_Number)) {
-    hsv.h -= h_diff;
-    RGB rgb = hsv_to_rgb(hsv);
-    rgb_matrix_set_color(11, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(22, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color(50, rgb.r, rgb.g, rgb.b);
-    return;
   }
 }
 
