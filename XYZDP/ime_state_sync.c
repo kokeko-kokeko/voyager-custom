524c70433ce8580d612ea4b901ebf155f7fed35d
kokeko-kokeko <kokeko@mac.com>
2025-08-28 11:49:07 +0900

    Update ime_state_sync.c

--- XYZDP/ime_state_sync.c
+++ XYZDP/ime_state_sync.c
@@ -11,4 +11,18 @@
 // use abs path
 #include "lib/lib8tion/lib8tion.h"
 
+#include "layer_num.h"
+
+// ime state from LANG1/LANG2 key
+static bool ime_on = false;
+static bool ime_kk = false;  //KataKana
+
+// Ime State Sync system
+static bool iss_enable = true; 
+static bool iss_sync = false;
+
+static fast_timer_t iss_trigger = 0;
+static const fast_timer_t iss_sync_delay = 15013; //ms
+static const fast_timer_t iss_idle_delay = 600011; //ms
+
 
