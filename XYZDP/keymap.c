02dc178895237d1df7e423dfbaa485b5669f86f2
kokeko-kokeko <kokeko@mac.com>
2025-08-28 12:40:33 +0900

    Update keymap.c

--- XYZDP/keymap.c
+++ XYZDP/keymap.c
@@ -437,6 +437,7 @@ extern bool is_launching;
 
 // split impl
 #include "fade_matrix.h"
+#include "ime_state_sync.h"
 #include "overlay_layer_mod.h"
 #include "status_led.h"
 
@@ -445,22 +446,7 @@ extern bool is_launching;
 // sub safety 14400000 ms (14400 sec = 240 min = 4 hour)
 static const fast_timer_t maximum_delay = (UINT32_MAX / 2) - 14400000;
 
-static void set_layer_color_overlay_base(void);
 static void set_layer_color_firmware_map_base(void);
-
-// ime state from LANG1/LANG2 key
-static bool ime_on = false;
-static bool ime_kk = false;  //KataKana
-
-// Ime State Sync system
-static bool iss_enable = true; 
-static bool iss_sync = false;
-
-static fast_timer_t iss_sync_trigger = 0;
-static const fast_timer_t iss_sync_delay = 15013; //ms
-
-static fast_timer_t iss_idle_trigger = 0;
-static const fast_timer_t iss_idle_delay = 600011; //ms
 
 // Ime State Display system
 
@@ -540,6 +526,7 @@ bool rgb_matrix_indicators_user(void) {
   switch (get_highest_layer(layer_state)) {
     case L_Firmware:
       set_layer_color_firmware_map_base();
+      set_layer_color_firmware_map_ime_state_sync();
       break;
     case L_Set_Speed:
       set_layer_color_speed_map();
@@ -558,7 +545,7 @@ bool rgb_matrix_indicators_user(void) {
         rgb_matrix_set_color_all(0, 0, 0);
       } else {
         set_layer_color_overlay_mod();
-        set_layer_color_overlay_base();
+        set_layer_color_overlay_iss_state_sync();
         set_layer_color_overlay_layer();
       }
       break;
@@ -688,19 +675,8 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 }
 
 bool pre_process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (iss_enable) {
-    if(iss_sync) {
-      iss_sync = false;
-      if (ime_on) {
-        if (ime_kk) {
-          tap_code16(LSFT(KC_LANGUAGE_1));
-        } else {
-          tap_code16(KC_LANGUAGE_1);
-        }
-      } else {
-        tap_code16(KC_LANGUAGE_2);
-      }
-    }
+  if (pre_process_record_ime_state_sync(keycode, record) == false) {
+    return false;
   }
   return true;
 }
@@ -710,11 +686,7 @@ void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
     fast_timer_t now = timer_read_fast();
 
     activate_fade_matrix(now);
-    
-    if (iss_enable) {
-      iss_sync_trigger = now + iss_sync_delay;
-      iss_idle_trigger = now + iss_idle_delay;
-    }
+    activate_ime_state_sync(now);
   }
   
   return;
@@ -725,17 +697,7 @@ void housekeeping_task_user(void) {
 
   update_fade_matrix(now);
   update_status_led(now);
-  
-  if (timer_expired_fast(now, iss_sync_trigger)) {
-    iss_sync_trigger += maximum_delay;
-    iss_sync = true;   
-  }
-  
-  if (timer_expired_fast(now, iss_idle_trigger)) {
-    iss_idle_trigger += maximum_delay;
-    iss_sync = false;
-    ime_on = false;
-  }
+  update_ime_state_sync(now);
   
   return;
 }
@@ -1250,7 +1212,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       return false;
     case HSV_215_255_102:
       if (record->event.pressed) {
-        iss_enable = true;
+        ime_state_sync_enable();
       }
       return false;
     case HSV_215_255_106:
@@ -1260,8 +1222,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       return false;
     case HSV_215_255_108:
       if (record->event.pressed) {
-        iss_enable = false;
-        iss_sync = false;
+        ime_state_sync_disable();
       }
       return false;
     case HSV_215_255_131:
@@ -2305,102 +2266,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         fade_matrix_step();
       }
       return false;
-
-    //IME state display (update flag & re-calc status)
-    case LT(L_Function, KC_LANGUAGE_1):
-      if (record->tap.count > 0) {
-        if (record->event.pressed) {
-          if (layer_state_is(L_LeftPinky) | layer_state_is(L_RightPinky) | layer_state_is(L_Function) |
-            layer_state_is(L_Number) | layer_state_is(L_Cursor)) {
-            // reverse side (upper layer)
-            if ((get_mods() & MOD_MASK_CSAG) == 0) {
-              ime_on = false;
-            }
-            tap_code16(KC_LANGUAGE_2);
-            return false;
-          } else {
-            // normal side (base layer)
-            if ((get_mods() & MOD_MASK_CAG) == 0) {
-              ime_on = true;
-              if (get_mods() & MOD_MASK_SHIFT) {
-                ime_kk = true;
-              } else {
-                ime_kk = false;
-              }
-            }
-          }
-        }
-      }
-      return true;
-    
-    case KC_LANGUAGE_2:
-      if (record->event.pressed) {
-        if (layer_state_is(L_LeftPinky) | layer_state_is(L_RightPinky) | layer_state_is(L_Function) |
-          layer_state_is(L_Number) | layer_state_is(L_Cursor)) {
-          // reverse side (upper layer)
-          if ((get_mods() & MOD_MASK_CAG) == 0) {
-            ime_on = true;
-            if (get_mods() & MOD_MASK_SHIFT) {
-              ime_kk = true;
-            } else {
-              ime_kk = false;
-            }
-          }
-          tap_code16(KC_LANGUAGE_1);
-          return false;
-        } else {
-          // normal side (base layer)
-          if ((get_mods() & MOD_MASK_CSAG) == 0) {
-            ime_on = false;
-          }
-        }
-      }
-      return true;
   }
+
+  if (process_record_ime_state_sync(keycode, record) == false) {
+    return false;
+  }
+  
   return true;
 }
 
 // local functions
 // basic static 
-static void set_layer_color_overlay_base(void) {
-  // use golden angle 255 * phi
-  const uint8_t h_diff = 97;
-  
-  HSV hsv = rgb_matrix_get_hsv();
-  hsv.h -= g_rgb_timer >> 4;
-  hsv.s = 255;
-  
-  // copy logic from breathing_anim.h 
-  // use different speed divide
-  uint8_t speed = rgb_matrix_get_speed(); 
-  uint16_t time = scale16by8(g_rgb_timer, speed / 3);
-  hsv.v = scale8(abs8(sin8(time) - 128) * 2, hsv.v);
-
-  // IME state sync syntem state
-  if (ime_on) {
-    hsv.h -= h_diff;
-    RGB rgb = hsv_to_rgb(hsv);
-    rgb_matrix_set_color(44, rgb.r, rgb.g, rgb.b);
-    
-    if (ime_kk) {
-      rgb_matrix_set_color(38, rgb.r, rgb.g, rgb.b);
-    } else {
-      rgb_matrix_set_color(38, 0, 0, 0);
-    }
-    
-    if (iss_sync) {
-      rgb_matrix_set_color(23, rgb.r, rgb.g, rgb.b);
-    } else {
-      rgb_matrix_set_color(23, 0, 0, 0);
-    }
-    rgb_matrix_set_color(5, 0, 0, 0);
-    rgb_matrix_set_color(11, 0, 0, 0);
-    rgb_matrix_set_color(17, 0, 0, 0);
-    rgb_matrix_set_color(26, 0, 0, 0);
-    rgb_matrix_set_color(32, 0, 0, 0);
-  }
-}
-
 void set_layer_color_firmware_map_base(void) {
   const uint8_t f = rgb_matrix_get_val();
   const uint8_t h = f >> 1;
@@ -2425,16 +2301,6 @@ void set_layer_color_firmware_map_base(void) {
     //ANSI base
     rgb_matrix_set_color(0, f, 0, 0);
     rgb_matrix_set_color(6, 0, o, 0);
-  }
-
-  //ISS
-  if (iss_enable) {
-    rgb_matrix_set_color(2, 0, f, 0);
-    rgb_matrix_set_color(8, o, o, o);
-  } else {
-    //ANSI base
-    rgb_matrix_set_color(2, o, o, o);
-    rgb_matrix_set_color(8, f, f, f);
   }
 
   //OS detect
