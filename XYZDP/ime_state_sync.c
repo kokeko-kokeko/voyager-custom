60aa04705e14d0b53367b43c1d08c53c64f52640
kokeko-kokeko <kokeko@mac.com>
2025-08-28 11:36:56 +0900

    Update ime_state_sync.c

--- XYZDP/ime_state_sync.c
+++ XYZDP/ime_state_sync.c
@@ -1,1 +1,14 @@
+#include QMK_KEYBOARD_H
+#include "version.h"
+#define MOON_LED_LEVEL LED_LEVEL
+#ifndef ZSA_SAFE_RANGE
+#define ZSA_SAFE_RANGE SAFE_RANGE
+#endif
+
+#include "overlay_layer_mod.h"
+
+// use animation logic
+// use abs path
+#include "lib/lib8tion/lib8tion.h"
+
 
