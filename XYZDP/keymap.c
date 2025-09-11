e3444a4a592e71c2d873a39e25b052e0c4433e91
kokeko-kokeko <kokeko@mac.com>
2025-09-11 20:56:05 +0900

    Update keymap.c

--- XYZDP/keymap.c
+++ XYZDP/keymap.c
@@ -936,14 +936,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         }  
       }  
       return false;
-  case NAVIGATOR_TURBO:
-    return false;
-  case NAVIGATOR_AIM:
-    return false;
-  case NAVIGATOR_INC_CPI:
-    return false;
-  case NAVIGATOR_DEC_CPI:
-    return false;
+    
+    //remove dummy mouse code
+    
     case RGB_SLD:
       if (record->event.pressed) {
         fade_matrix_set_mode(RGB_MATRIX_SOLID_COLOR);
@@ -2009,6 +2004,44 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case RGB_MODE_FORWARD:
       if (record->event.pressed) {
         fade_matrix_step();
+      }
+      return false;
+
+    // mouse code
+    case DRAG_SCROLL:
+      if (record->event.pressed) {
+        set_scrolling = true;
+      } else {
+        set_scrolling = false;
+      }
+      return false;
+    case TOGGLE_SCROLL:
+      if (record->event.pressed) {
+        set_scrolling = !set_scrolling;
+      }
+      return false;
+    case NAVIGATOR_TURBO:
+      if (record->event.pressed) {
+        navigator_turbo = true;
+      } else {
+        navigator_turbo = false;
+      }
+      return false;
+    case NAVIGATOR_AIM:
+      if (record->event.pressed) {
+        navigator_aim = true;
+      } else {
+        navigator_aim = false;
+      }
+      return false;
+    case NAVIGATOR_INC_CPI:
+      if (record->event.pressed) {
+        pointing_device_set_cpi(1);
+      }
+      return false;
+    case NAVIGATOR_DEC_CPI:
+      if (record->event.pressed) {
+        pointing_device_set_cpi(0);
       }
       return false;
   }
