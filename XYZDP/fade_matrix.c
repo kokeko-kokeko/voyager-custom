4ef026a92ef379262f33b13c7c6e3cc3addf3981
kokeko-kokeko <kokeko@mac.com>
2025-09-18 18:14:05 +0900

    Update fade_matrix.c

--- XYZDP/fade_matrix.c
+++ XYZDP/fade_matrix.c
@@ -502,8 +502,8 @@ void set_layer_color_mouse_map(void) {
   // right side
   rgb_matrix_set_color(26, f, f, 0);
   rgb_matrix_set_color(32, 0, 0, f);
-  rgb_matrix_set_color(38, f, f, f);
-  rgb_matrix_set_color(44, 0, o, o);
+  //rgb_matrix_set_color(38, f, f, f);
+  //rgb_matrix_set_color(44, 0, o, o);
   rgb_matrix_set_color(45, f, f, f);
   
   // Aim Turbo
