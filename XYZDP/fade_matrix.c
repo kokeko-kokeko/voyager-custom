a0446698c3e4da2e4ee87a953c4aa9d88b70e60f
kokeko-kokeko <kokeko@mac.com>
2025-09-24 19:10:44 +0900

    Update fade_matrix.c

--- XYZDP/fade_matrix.c
+++ XYZDP/fade_matrix.c
@@ -485,26 +485,19 @@ void set_layer_color_mouse_map(void) {
   rgb_matrix_set_color_all(0, 0, 0);
 
   // left side
-  
-  
   rgb_matrix_set_color( 5, 0, f, f);
   rgb_matrix_set_color(11, f, f, 0);
   rgb_matrix_set_color(17, 0, 0, f);
   rgb_matrix_set_color(22, f, f, f);
-  //rgb_matrix_set_color(23, o, o, o);
   rgb_matrix_set_color(24, 0, f, 0);
   rgb_matrix_set_color(25, f, 0, 0);
 
-  // CPI
-  //rgb_matrix_set_color(18, o, o, 0);
-  //rgb_matrix_set_color(19, o, 0, o);
-  //rgb_matrix_set_color(20, f, 0, f);
-
   // right side
   rgb_matrix_set_color(26, 0, f, f);
-  rgb_matrix_set_color(32, f, f, 0);
-  rgb_matrix_set_color(38, 0, 0, f);
-  //gb_matrix_set_color(44, o, o, o);
+  rgb_matrix_set_color(32, 0, 0, f);
+  rgb_matrix_set_color(33, f, 0, 0);
+  rgb_matrix_set_color(34, 0, f, 0);
+  rgb_matrix_set_color(38, f, f, 0);
   rgb_matrix_set_color(45, f, f, f);
   rgb_matrix_set_color(50, f, f, f);
   
