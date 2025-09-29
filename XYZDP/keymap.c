9bc06289f0cf6e69fdeb682106a843960eaee223
kokeko-kokeko <kokeko@mac.com>
2025-09-29 15:27:21 +0900

    Update keymap.c

--- XYZDP/keymap.c
+++ XYZDP/keymap.c
@@ -2359,10 +2359,10 @@ void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
         if (record->event.key.row < MATRIX_ROWS / 2) {
           // left side mouse button
           if (record->event.pressed) {
-            mouse_button_press_time [keycode - KC_MS_BTN1] = record->event.time;
+            mouse_button_press_time[keycode - KC_MS_BTN1] = record->event.time;
             // early trigger reset on auto_mouse_activation
           } else {
-            uint16_t mouse_button_duration = record->event.time - mouse_button_press_time [keycode - KC_MS_BTN1];
+            uint16_t mouse_button_duration = record->event.time - mouse_button_press_time[keycode - KC_MS_BTN1];
             if (mouse_button_duration < AUTO_MOUSE_DRAG_THRESHOLD) {
               fast_timer_t now = timer_read_fast();
               auto_mouse_early_trigger = now + AUTO_MOUSE_TIME_LEFT;
@@ -2371,10 +2371,10 @@ void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
         } else {
           // right side mouse button
           if (record->event.pressed) {
-            mouse_button_press_time [keycode - KC_MS_BTN1 + 8] = record->event.time;
+            mouse_button_press_time[keycode - KC_MS_BTN1 + 8] = record->event.time;
             // early trigger reset on auto_mouse_activation
           } else {
-            uint16_t mouse_button_duration = record->event.time - mouse_button_press_time [keycode - KC_MS_BTN1 + 8];
+            uint16_t mouse_button_duration = record->event.time - mouse_button_press_time[keycode - KC_MS_BTN1 + 8];
             if (mouse_button_duration < AUTO_MOUSE_DRAG_THRESHOLD) {
               fast_timer_t now = timer_read_fast();
               auto_mouse_early_trigger = now + AUTO_MOUSE_TIME_RIGHT;
