20aa0ba7ce9f42d25d79461f344ba36e49b7f770
kokeko-kokeko <kokeko@mac.com>
2025-10-04 14:17:44 +0900

    Update keymap.c

--- XYZDP/keymap.c
+++ XYZDP/keymap.c
@@ -2359,9 +2359,11 @@ void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
         if (record->event.key.row < MATRIX_ROWS / 2) {
           // left side mouse button
           if (record->event.pressed) {
+            navigator_aim = true;
             mouse_button_press_time[keycode - KC_MS_BTN1] = record->event.time;
             // early trigger reset on auto_mouse_activation
           } else {
+            navigator_aim = false:
             uint16_t mouse_button_duration = record->event.time - mouse_button_press_time[keycode - KC_MS_BTN1];
             if (mouse_button_duration < AUTO_MOUSE_DRAG_THRESHOLD) {
               fast_timer_t now = timer_read_fast();
@@ -2371,9 +2373,11 @@ void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
         } else {
           // right side mouse button
           if (record->event.pressed) {
+            navigator_turbo = true;
             mouse_button_press_time[keycode - KC_MS_BTN1 + 8] = record->event.time;
             // early trigger reset on auto_mouse_activation
           } else {
+            navigator_turbo = false;
             uint16_t mouse_button_duration = record->event.time - mouse_button_press_time[keycode - KC_MS_BTN1 + 8];
             if (mouse_button_duration < AUTO_MOUSE_DRAG_THRESHOLD) {
               fast_timer_t now = timer_read_fast();
