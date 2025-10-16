065d3d4e52979dc7007eb4ef90c30ebe4324cc8e
kokeko-kokeko <kokeko@mac.com>
2025-10-16 15:38:52 +0900

    Update keymap.c
    static more small scope test

--- XYZDP/keymap.c
+++ XYZDP/keymap.c
@@ -2384,7 +2384,7 @@ void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
 
   // button max count
-  static uint16_t mouse_button_press_time[16];
+  //static uint16_t mouse_button_press_time[16];
 
   // early auto mouse timeout
   if (is_auto_mouse_active()) {
@@ -2398,28 +2398,30 @@ void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
       case KC_MS_BTN7:
       case KC_MS_BTN8:
         if (record->event.key.row < MATRIX_ROWS / 2) {
+           static uint16_t mouse_button_press_time_left[8];
           // left side mouse button
           if (record->event.pressed) {
             //navigator_aim = true;
-            mouse_button_press_time[keycode - KC_MS_BTN1] = record->event.time;
+            mouse_button_press_time_left[keycode - KC_MS_BTN1] = record->event.time;
             // early trigger reset on auto_mouse_activation
           } else {
             //navigator_aim = false;
-            uint16_t mouse_button_duration = record->event.time - mouse_button_press_time[keycode - KC_MS_BTN1];
+            uint16_t mouse_button_duration = record->event.time - mouse_button_press_time_left[keycode - KC_MS_BTN1];
             if (mouse_button_duration < AUTO_MOUSE_DRAG_THRESHOLD) {
               fast_timer_t now = timer_read_fast();
               auto_mouse_early_trigger = now + AUTO_MOUSE_TIME_LEFT_SIDE;
             }
           }
         } else {
+          static uint16_t mouse_button_press_time_right[8];
           // right side mouse button
           if (record->event.pressed) {
             //navigator_turbo = true;
-            mouse_button_press_time[keycode - KC_MS_BTN1 + 8] = record->event.time;
+            mouse_button_press_time_right[keycode - KC_MS_BTN1] = record->event.time;
             // early trigger reset on auto_mouse_activation
           } else {
             //navigator_turbo = false;
-            uint16_t mouse_button_duration = record->event.time - mouse_button_press_time[keycode - KC_MS_BTN1 + 8];
+            uint16_t mouse_button_duration = record->event.time - mouse_button_press_time_right[keycode - KC_MS_BTN1];
             if (mouse_button_duration < AUTO_MOUSE_DRAG_THRESHOLD) {
               fast_timer_t now = timer_read_fast();
               auto_mouse_early_trigger = now + AUTO_MOUSE_TIME_RIGHT_SIDE;
