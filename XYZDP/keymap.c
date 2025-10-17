8702cfca1f3efb876adf9bae636a62f00d05b59b
kokeko-kokeko <kokeko@mac.com>
2025-10-18 03:02:48 +0900

    Update keymap.c

--- XYZDP/keymap.c
+++ XYZDP/keymap.c
@@ -2281,10 +2281,13 @@ layer_state_t layer_state_set_user(layer_state_t state) {
   // call Hue with Sat and Val
   state = update_tri_layer_state(state, L_Set_Val, L_Set_Sat, L_Set_Hue);
   
-  //ANSI/JIS addiional enable
+  // ANSI/JIS addiional enable
   state = update_tri_layer_state(state, L_Base_JIS, L_Number, L_Number_JIS);
   state = update_tri_layer_state(state, L_Base_JIS, L_Cursor, L_Cursor_JIS);
   state = update_tri_layer_state(state, L_Base_JIS, L_BothThumb, L_BothThumb_JIS);
+
+  // cursor & mouse override
+  state = update_tri_layer_state(state, L_Cursor, L_Mouse, L_Cursor_Mouse_Override);
 
   // base layer scroll lock
   static bool mouse_setting_on = false;
@@ -2362,6 +2365,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     case L_BothPinkyThumb:
       break;
     case L_Mouse:
+    case L_Cursor_Mouse_Override:
       // mouse indication
       status_led(now, 0b0011, led_pattern_off);
       status_led(now, 0b0100, led_pattern_on);
@@ -2412,6 +2416,7 @@ bool rgb_matrix_indicators_user(void) {
   if (keyboard_config.disable_layer_led) { return false; }
   switch (get_highest_layer(layer_state)) {
     case L_Mouse:
+    case L_Cursor_Mouse_Override:
       set_layer_color_mouse_map();
       break;    
     case L_Firmware:
