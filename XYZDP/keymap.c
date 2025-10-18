c831ba49cf1bcc3ed6f5a6df7bef9ca39b83112c
kokeko-kokeko <kokeko@mac.com>
2025-10-18 21:56:31 +0900

    Update keymap.c

--- XYZDP/keymap.c
+++ XYZDP/keymap.c
@@ -2095,7 +2095,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     // auto mouse EXIT keys (dummy keycode)
     case KC_LANGUAGE_6:
       if (record->event.pressed) {
-        auto_mouse_layer_off();
+        fast_timer_t now = timer_read_fast();
+        auto_mouse_early_trigger = now + AUTO_MOUSE_TIME_EXIT;
       }
       return false;
   }
