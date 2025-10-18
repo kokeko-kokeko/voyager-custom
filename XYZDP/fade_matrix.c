e67e1b508273062e847d5a1609e4eba70f22438e
kokeko-kokeko <kokeko@mac.com>
2025-10-18 19:05:16 +0900

    Update fade_matrix.c

--- XYZDP/fade_matrix.c
+++ XYZDP/fade_matrix.c
@@ -477,19 +477,18 @@ void set_layer_color_firmware_map(void) {
 
   //color test
   rgb_matrix_set_color(26, f, 0, 0);
-  rgb_matrix_set_color(27, f, h, 0);
-  rgb_matrix_set_color(28, f, f, 0);
-
-  rgb_matrix_set_color(32, h, f, 0);
-  rgb_matrix_set_color(33, 0, f, 0);
-  rgb_matrix_set_color(34, 0, f, h);
-
-  rgb_matrix_set_color(38, 0, f, f);
-  rgb_matrix_set_color(39, 0, h, f);
-  rgb_matrix_set_color(40, 0, 0, f);
-
-  rgb_matrix_set_color(44, h, 0, f);
-  rgb_matrix_set_color(45, f, 0, f);
+  rgb_matrix_set_color(32, f, h, 0);
+  rgb_matrix_set_color(38, f, f, 0);
+  rgb_matrix_set_color(44, h, f, 0);
+  
+  rgb_matrix_set_color(27, 0, f, 0);
+  rgb_matrix_set_color(33, 0, f, h);
+  rgb_matrix_set_color(39, 0, f, f);
+  rgb_matrix_set_color(45, 0, h, f);
+  
+  rgb_matrix_set_color(28, 0, 0, f);
+  rgb_matrix_set_color(34, h, 0, f);
+  rgb_matrix_set_color(40, f, 0, f);
   rgb_matrix_set_color(46, f, 0, h);
 }
 
