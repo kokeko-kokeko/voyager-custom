a47b79a9da61e2379c49b4045ff9daf9e969f514
kokeko-kokeko <kokeko@mac.com>
2025-10-19 14:16:15 +0900

    Update keymap.c

--- XYZDP/keymap.c
+++ XYZDP/keymap.c
@@ -2388,38 +2388,39 @@ void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
       case KC_MS_BTN6:
       case KC_MS_BTN7:
       case KC_MS_BTN8:
-        if (record->event.key.row < MATRIX_ROWS / 2) {
-          // left side mouse button
-          static uint16_t press_time[8];
-          if (record->event.pressed) {
-            //navigator_aim = true;
-            press_time[keycode - KC_MS_BTN1] = record->event.time;
-            // early trigger reset on auto_mouse_activation
-          } else {
-            //navigator_aim = false;
-            uint16_t duration = record->event.time - press_time[keycode - KC_MS_BTN1];
-            if (duration < AUTO_MOUSE_DRAG_THRESHOLD) {
-              fast_timer_t now = timer_read_fast();
-              auto_mouse_early_trigger = now + AUTO_MOUSE_TIME_LEFT_SIDE;
+        {
+          if (record->event.key.row < MATRIX_ROWS / 2) {
+            // left side mouse button
+            static uint16_t press_time[8];
+            if (record->event.pressed) {
+              press_time[keycode - KC_MS_BTN1] = record->event.time;
+              // early trigger reset on auto_mouse_activation
+            } else {
+              //navigator_aim = false;
+              uint16_t duration = record->event.time - press_time[keycode - KC_MS_BTN1];
+              if (duration < AUTO_MOUSE_DRAG_THRESHOLD) {
+                fast_timer_t now = timer_read_fast();
+                auto_mouse_early_trigger = now + AUTO_MOUSE_TIME_LEFT_SIDE;
+              }
             }
-          }
-        } else {
-          // right side mouse button
-          static uint16_t press_time[8];
-          if (record->event.pressed) {
-            //navigator_turbo = true;
-            press_time[keycode - KC_MS_BTN1] = record->event.time;
-            // early trigger reset on auto_mouse_activation
           } else {
-            //navigator_turbo = false;
-            uint16_t duration = record->event.time - press_time[keycode - KC_MS_BTN1];
-            if (duration < AUTO_MOUSE_DRAG_THRESHOLD) {
-              fast_timer_t now = timer_read_fast();
-              auto_mouse_early_trigger = now + AUTO_MOUSE_TIME_RIGHT_SIDE;
+            // right side mouse button
+            static uint16_t press_time[8];
+            if (record->event.pressed) {
+              //navigator_turbo = true;
+              press_time[keycode - KC_MS_BTN1] = record->event.time;
+              // early trigger reset on auto_mouse_activation
+            } else {
+              //navigator_turbo = false;
+              uint16_t duration = record->event.time - press_time[keycode - KC_MS_BTN1];
+              if (duration < AUTO_MOUSE_DRAG_THRESHOLD) {
+                fast_timer_t now = timer_read_fast();
+                auto_mouse_early_trigger = now + AUTO_MOUSE_TIME_RIGHT_SIDE;
+              }
             }
           }
         }
-        break;
+        break; 
     }
   }
   
