4120d5886e745ce393d74c91a04f75d17aa2d290
kokeko-kokeko <kokeko@mac.com>
2025-10-22 00:51:18 +0900

    Update fade_matrix.c

--- XYZDP/fade_matrix.c
+++ XYZDP/fade_matrix.c
@@ -298,7 +298,7 @@ void set_layer_color_hue_map(void) {
   rgb_matrix_set_color(24, rgb.r, rgb.g, rgb.b);
   rgb_matrix_set_color(25, rgb.r, rgb.g, rgb.b);
   rgb_matrix_set_color(50, hsv.v, hsv.v, hsv.v);
-  rgb_matrix_set_color(51, hsv.v, 0, 0);
+  rgb_matrix_set_color(51, hsv.v, hsv.v, 0);
   uint8_t key = hsv.h;
   uint8_t i = 0;
   for (i = 0; i < 48; i++) {
@@ -331,7 +331,7 @@ void set_layer_color_sat_map(void) {
 
   rgb_matrix_set_color(25, rgb.r, rgb.g, rgb.b);
   rgb_matrix_set_color(50, hsv.v, hsv.v, hsv.v);
-  rgb_matrix_set_color(51, 0, 0, 0);
+  rgb_matrix_set_color(51, 0, hsv.v, 0);
   uint8_t key = hsv.s;
   uint8_t i = 0;
   for (i = 0; i < 48; i++) {
@@ -364,7 +364,7 @@ void set_layer_color_val_map(void) {
 
   rgb_matrix_set_color(24, rgb.r, rgb.g, rgb.b);
   rgb_matrix_set_color(50, hsv.v, hsv.v, hsv.v);
-  rgb_matrix_set_color(51, 0, 0, 0);
+  rgb_matrix_set_color(51, 0, 0, hsv.v);
   uint8_t key = hsv.v;
   uint8_t i = 0;
   for (i = 0; i < 48; i++) {
@@ -396,7 +396,7 @@ void set_layer_color_speed_map(void) {
   RGB rgb = hsv_to_rgb(hsv);
 
   rgb_matrix_set_color(50, hsv.v, hsv.v, hsv.v);
-  rgb_matrix_set_color(51, 0, hsv.v, 0);
+  rgb_matrix_set_color(51, 0, 0, 0);
   
   // copy logic from breathing_anim.h   
   uint16_t time = scale16by8(g_rgb_timer, rgb_matrix_config.speed / 8);
