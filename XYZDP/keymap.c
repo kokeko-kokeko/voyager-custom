ca9dcbcfe1ef3ce9673ad81b7cd73ec9b47e4e9a
kokeko-kokeko <kokeko@mac.com>
2025-10-23 16:29:14 +0900

    Update keymap.c

--- XYZDP/keymap.c
+++ XYZDP/keymap.c
@@ -909,6 +909,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         } else if (pos == 50) {
           layer_on(L_Set_Hue);
         } else if (pos == 51) {
+          set_auto_mouse_enable(false);
           clear_keyboard();
           layer_on(L_Halt_Mask);
         }
@@ -1037,6 +1038,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         if (52 <= pos) return false;
         
         if (pos == 49) {
+          while (1);
 
         } else if (pos == 50) {
           // off all setting layers
@@ -1046,6 +1048,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           layer_off(L_Set_Sat);
           layer_off(L_Set_Hue);
           layer_off(L_Firmware);
+
+          set_auto_mouse_enable(true);
           
           status_led(now, 0b1111, led_pattern_oneshot);
         } else {
