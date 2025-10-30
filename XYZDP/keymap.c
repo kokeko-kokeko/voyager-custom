2fef41a269c8aa97444808df9371d0ae595219d2
kokeko-kokeko <kokeko@mac.com>
2025-10-30 16:49:40 +0900

    Update keymap.c

--- XYZDP/keymap.c
+++ XYZDP/keymap.c
@@ -1196,32 +1196,31 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
   
   // auto mouse EXIT key right (dummy keycode)
-  switch (keycode) {    
-    case KC_LANGUAGE_7:
-      if (record->event.pressed) {
-        exit_right_hand_press_time = record->event.time;
-        set_scrolling = true;
+  if (keycode == KC_LANGUAGE_7) {
+    if (record->event.pressed) {
+      exit_right_hand_press_time = record->event.time;
+      set_scrolling = true;
+    } else {
+      if (TIMER_DIFF_16(record->event.time, exit_right_hand_press_time) < AUTO_MOUSE_DRAG_THRESHOLD) {
+        // tap to exit auto mouse
+        set_scrolling = false;
+        lock_scrolling = false;
+        auto_mouse_early_off_trigger = now_buffer + 1;
       } else {
-        if (TIMER_DIFF_16(record->event.time, exit_right_hand_press_time) < AUTO_MOUSE_DRAG_THRESHOLD) {
-          // tap to exit auto mouse
-          set_scrolling = false;
-          lock_scrolling = false;
-          auto_mouse_early_off_trigger = now_buffer + 1;
-        } else {
-          // drag, must release lock non exit
-          set_scrolling = false;
-          lock_scrolling = false;
-        }
+        // drag, must release lock non exit
+        set_scrolling = false;
+        lock_scrolling = false;
       }
-      // update LED
-      if (set_scrolling) {
-        status_led(now_buffer, 0b0100, led_pattern_on);
-      } else {
-        status_led(now_buffer, 0b0100, led_pattern_off);
-      }
-      return false;
+    }
+    // update LED
+    if (set_scrolling) {
+      status_led(now_buffer, 0b0100, led_pattern_on);
+    } else {
+      status_led(now_buffer, 0b0100, led_pattern_off);
+    }
+    return false;
   }
-
+  
   // mouse button eraly exit
   switch (keycode) {  
     case KC_MS_BTN1:
