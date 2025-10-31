b6b9b736404d21e36af4faded193c6599f3eb551
kokeko-kokeko <kokeko@mac.com>
2025-10-31 14:22:08 +0900

    Update keymap.c

--- XYZDP/keymap.c
+++ XYZDP/keymap.c
@@ -326,32 +326,33 @@ void pointing_device_init_user(void) {
 }
 
 bool is_mouse_record_user(uint16_t keycode, keyrecord_t* record) {
-//  // Treat all keys as mouse keys when in the automouse layer so that any key set resets the timeout without leaving the layer.
-//  if (!layer_state_is(AUTO_MOUSE_TARGET_LAYER)){
-//    // When depressing a mouse key with a LT key at the same time, the mouse key tracker is not decremented.
-//    // This is a workaround to fix that
-//    if (IS_MOUSE_KEYCODE(keycode) && !record->event.pressed) {
-//      return true;
-//    }
-//    return false;
-//  }
-//  return true;
-
-  // disable oryx logic, from old is_mouse_record_kb logic and self hold
-  switch (keycode) {
-    case NAVIGATOR_INC_CPI ... NAVIGATOR_AIM:
-    case DRAG_SCROLL:
-    case KC_LANGUAGE_6:
-    case KC_LANGUAGE_7:
+  // Treat all keys as mouse keys when in the automouse layer so that any key set resets the timeout without leaving the layer.
+  if (!layer_state_is(AUTO_MOUSE_TARGET_LAYER)){
+    // When depressing a mouse key with a LT key at the same time, the mouse key tracker is not decremented.
+    // This is a workaround to fix that
+    if (IS_MOUSE_KEYCODE(keycode) && !record->event.pressed) {
       return true;
+    }
+    return false;
   }
+  return true;
+}
+
+//  // disable oryx logic, from old is_mouse_record_kb logic and self hold
+//  switch (keycode) {
+//    case NAVIGATOR_INC_CPI ... NAVIGATOR_AIM:
+//    case DRAG_SCROLL:
+//    case KC_LANGUAGE_6:
+//    case KC_LANGUAGE_7:
+//      return true;
+//  }
 
   // only self hold override layer
-  if (layer_state_is(L_Mouse_Number_Override)) return true;
-  if (layer_state_is(L_Mouse_Cursor_Override)) return true;
+//  if (layer_state_is(L_Mouse_Number_Override)) return true;
+//  if (layer_state_is(L_Mouse_Cursor_Override)) return true;
   
-  return false;
-}
+//  return false;
+//}
 
 
 
