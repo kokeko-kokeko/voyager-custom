b1f8092f94ba34c5f6569a1e5ab969995b1fe3e0
kokeko-kokeko <kokeko@mac.com>
2025-10-31 12:51:48 +0900

    Update keymap.c

--- XYZDP/keymap.c
+++ XYZDP/keymap.c
@@ -1300,8 +1300,7 @@ bool process_record_mouse(uint16_t keycode, keyrecord_t *record) {
 void post_process_record_mouse(uint16_t keycode, keyrecord_t *record) {
   static uint16_t btn_left_hand_press_time[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
   static uint16_t btn_right_hand_press_time[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
-
-  //if (is_auto_mouse_active() == false) return;
+  
   if (IS_MOUSEKEY_BUTTON(keycode) == false) return; 
   
   // mouse button eraly exit
