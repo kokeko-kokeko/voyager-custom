305209bfb856c13d9ca39c7769478905a68b4afb
kokeko-kokeko <kokeko@mac.com>
2025-11-01 19:41:05 +0900

    Update fade_matrix.c

--- XYZDP/fade_matrix.c
+++ XYZDP/fade_matrix.c
@@ -209,7 +209,7 @@ void fade_matrix_step(void) {
 void fade_matrix_load_preset(void) {
   fade_matrix_target.enable = true;
   fade_matrix_target.hsv.h = 250;
-  fade_matrix_target.hsv.s = 171;
+  fade_matrix_target.hsv.s = 180;
   fade_matrix_target.hsv.v = 109;
   fade_matrix_target.speed = 80;
   fade_matrix_target.mode = RGB_MATRIX_FLOWER_BLOOMING;
