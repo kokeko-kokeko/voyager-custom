e10ef79d53472a64efdafc1bb5ac70c50c28b392
kokeko-kokeko <kokeko@mac.com>
2025-11-06 11:42:32 +0900

    Update overlay_layer_mod.h

--- XYZDP/overlay_layer_mod.h
+++ XYZDP/overlay_layer_mod.h
@@ -1,4 +1,4 @@
 #pragma once
 
-extern void set_layer_color_overlay_layer(void);
-extern void set_layer_color_overlay_mod(void);
+void set_layer_color_overlay_layer(void);
+void set_layer_color_overlay_mod(void);
