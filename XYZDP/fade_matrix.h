b8f79d8922a1e07b8f8ff8c73e027700bc4bc8dc
kokeko-kokeko <kokeko@mac.com>
2025-11-06 12:52:35 +0900

    Update fade_matrix.h

--- XYZDP/fade_matrix.h
+++ XYZDP/fade_matrix.h
@@ -3,10 +3,10 @@
 uint8_t get_pos_from_keyrecord(const keyrecord_t * const record);
 
 void fade_matrix_set_mode(const uint8_t mode);
-void fade_matrix_set_hue_pos(const uint8_t pos);
-void fade_matrix_set_sat_pos(const uint8_t pos);
-void fade_matrix_set_val_pos(const uint8_t pos);
-void fade_matrix_set_speed_pos(const uint8_t pos);
+void fade_matrix_set_hue_keyrecord(const keyrecord_t * const record);
+void fade_matrix_set_sat_keyrecord(const keyrecord_t * const record);
+void fade_matrix_set_val_keyrecord(const keyrecord_t * const record);
+void fade_matrix_set_speed_keyrecord(const keyrecord_t * const record);
 
 void fade_matrix_increase_hue(void);
 void fade_matrix_decrease_hue(void);
