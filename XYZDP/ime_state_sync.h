3ff6a91fcbc1d4795cc981f15f9a0db2016db4b0
kokeko-kokeko <kokeko@mac.com>
2025-11-06 11:41:58 +0900

    Update ime_state_sync.h

--- XYZDP/ime_state_sync.h
+++ XYZDP/ime_state_sync.h
@@ -1,14 +1,14 @@
 #pragma once
 
-extern void ime_state_sync_enable(void);
-extern void ime_state_sync_disable(void);
+void ime_state_sync_enable(void);
+void ime_state_sync_disable(void);
 
-extern bool pre_process_record_ime_state_sync(uint16_t keycode, keyrecord_t *record);
-extern bool process_record_ime_state_sync(uint16_t keycode, keyrecord_t *record);
+bool pre_process_record_ime_state_sync(uint16_t keycode, keyrecord_t *record);
+bool process_record_ime_state_sync(uint16_t keycode, keyrecord_t *record);
 
-extern void activate_ime_state_sync(const fast_timer_t now);
-extern void update_ime_state_sync(const fast_timer_t now);
+void activate_ime_state_sync(const fast_timer_t now);
+void update_ime_state_sync(const fast_timer_t now);
 
-extern void set_layer_color_overlay_ime_state_sync(void);
+void set_layer_color_overlay_ime_state_sync(void);
 
-extern void set_layer_color_firmware_map_ime_state_sync(void);
+void set_layer_color_firmware_map_ime_state_sync(void);
