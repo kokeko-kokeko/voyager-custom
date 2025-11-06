856e8ea36f77dc89756a8e9be6346ee4a932331c
kokeko-kokeko <kokeko@mac.com>
2025-11-06 11:45:54 +0900

    Update status_led.h

--- XYZDP/status_led.h
+++ XYZDP/status_led.h
@@ -1,10 +1,10 @@
 #pragma once
 
-const uint8_t led_pattern_off[];
-const uint8_t led_pattern_on[];
-const uint8_t led_pattern_blink[];
-const uint8_t led_pattern_single[];
-const uint8_t led_pattern_oneshot[];
+extern const uint8_t led_pattern_off[];
+extern const uint8_t led_pattern_on[];
+extern const uint8_t led_pattern_blink[];
+extern const uint8_t led_pattern_single[];
+extern const uint8_t led_pattern_oneshot[];
 
 void status_led(const fast_timer_t now, const uint8_t mask, const uint8_t * const pattern);
 void update_status_led(const fast_timer_t now);
