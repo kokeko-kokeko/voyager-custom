7d8e13f49a6f657d02a1e3a47e9140a17f15ee97
kokeko-kokeko <kokeko@mac.com>
2025-11-25 18:17:02 +0900

    Update fade_matrix.c

--- XYZDP/fade_matrix.c
+++ XYZDP/fade_matrix.c
@@ -215,7 +215,7 @@ void fade_matrix_step(const fast_timer_t now) {
 
 void fade_matrix_load_preset(const fast_timer_t now) {
   fade_matrix_target.enable = true;
-  fade_matrix_target.hsv.h = 250;
+  fade_matrix_target.hsv.h = 86;
   fade_matrix_target.hsv.s = 128;
   fade_matrix_target.hsv.v = 105;
   fade_matrix_target.speed = 80;
