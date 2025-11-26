3addb0950483fdd3e9ddc7fc64e15357a0587f4c
kokeko-kokeko <kokeko@mac.com>
2025-11-26 17:54:21 +0900

    Update fade_matrix.c

--- XYZDP/fade_matrix.c
+++ XYZDP/fade_matrix.c
@@ -605,7 +605,7 @@ void set_layer_color_mouse_map(void) {
   const uint8_t f = rgb_matrix_get_val();
   const uint8_t h = f >> 1;
   const uint8_t q = h >> 1;
-  //const uint8_t o = q >> 1;
+  const uint8_t o = q >> 1;
 
   rgb_matrix_set_color_all(0, 0, 0);
 
@@ -631,7 +631,8 @@ void set_layer_color_mouse_map(void) {
   rgb_matrix_set_color(31, h, 0, f);
 
   rgb_matrix_set_color(32, 0, 0, f);
-    
+  rgb_matrix_set_color(37, o, q, 0);
+  
   rgb_matrix_set_color(38, f, 0, 0);
 
   //rgb_matrix_set_color(44, q, q, q);
