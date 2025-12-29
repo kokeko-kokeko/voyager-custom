a4626145226def142e37ad5fdf3037ebfc73f360
kokeko-kokeko <kokeko@mac.com>
2025-12-29 12:29:51 +0900

    Update ime_state_sync.h

--- XYZDP/ime_state_sync.h
+++ XYZDP/ime_state_sync.h
@@ -5,9 +5,9 @@ void ime_state_sync_disable(void);
 
 bool pre_process_record_ime_state_sync(uint16_t keycode, keyrecord_t *record);
 bool process_record_ime_state_sync(uint16_t keycode, keyrecord_t *record);
+void post_process_record_ime_state_sync(uint16_t keycode, keyrecord_t *record);
 
-void activate_ime_state_sync(const fast_timer_t now);
-void update_ime_state_sync(const fast_timer_t now);
+void housekeeping_ime_state_sync(void);
 
 void set_layer_color_overlay_ime_state_sync(void);
 
