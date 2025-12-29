039748fddcf605cc45ae940ec198eb95bbee9129
kokeko-kokeko <kokeko@mac.com>
2025-12-29 16:55:23 +0900

    Update adv_mouse.h

--- XYZDP/adv_mouse.h
+++ XYZDP/adv_mouse.h
@@ -5,3 +5,5 @@ void post_process_record_adv_mouse(uint16_t keycode, keyrecord_t *record);
 report_mouse_t pointing_device_task_adv_mouse(report_mouse_t mouse_report);
 layer_state_t layer_state_set_adv_mouse(const layer_state_t state);
 void housekeeping_task_adv_mouse(void);
+
+void set_layer_color_mouse_map(void);
