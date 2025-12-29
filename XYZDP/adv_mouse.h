c175b3ec39fcdb923cafb4ae7e602d052c8fc26c
kokeko-kokeko <kokeko@mac.com>
2025-12-29 15:56:39 +0900

    Update adv_mouse.h

--- XYZDP/adv_mouse.h
+++ XYZDP/adv_mouse.h
@@ -1,3 +1,6 @@
 #pragma once
 
-
+void keyboard_post_init_adv_mouse(void);
+void post_process_record_adv_mouse(uint16_t keycode, keyrecord_t *record);
+layer_state_t layer_state_set_adv_mouse(const layer_state_t state);
+void housekeeping_task_adv_mouse(void);
