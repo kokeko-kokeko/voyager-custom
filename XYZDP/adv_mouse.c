30002d59f276cc73c55ac628a92a68f538eed99a
kokeko-kokeko <kokeko@mac.com>
2025-12-29 15:30:28 +0900

    Create adv_mouse.c

---
+++ XYZDP/adv_mouse.c
@@ -0,0 +1,10 @@
+#include QMK_KEYBOARD_H
+#include "version.h"
+#define MOON_LED_LEVEL LED_LEVEL
+#ifndef ZSA_SAFE_RANGE
+#define ZSA_SAFE_RANGE SAFE_RANGE
+#endif
+
+#include "adv_mouse.h"
+
+#include "layer_num.h"
