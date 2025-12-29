96ef30fd110ec17b82e8f971c1c9696cbf9950e4
kokeko-kokeko <kokeko@mac.com>
2025-12-29 17:08:38 +0900

    Update adv_mouse.h

--- XYZDP/adv_mouse.h
+++ XYZDP/adv_mouse.h
@@ -1,5 +1,12 @@
 #pragma once
 
+#define AUTO_MOUSE_TIME_SHORT 499
+#define AUTO_MOUSE_TIME_MID 1999
+#define AUTO_MOUSE_TIME_LONG 30011
+
+#define AUTO_MOUSE_DRAG_THRESHOLD 599
+#define AUTO_MOUSE_MULTI_TAP_THRESHOLD 797
+
 void keyboard_post_init_adv_mouse(void);
 void post_process_record_adv_mouse(uint16_t keycode, keyrecord_t *record);
 report_mouse_t pointing_device_task_adv_mouse(report_mouse_t mouse_report);
