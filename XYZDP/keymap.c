9d0ec4e6b01984beed9b8e0ccc9b0bb3141197f5
kokeko-kokeko <kokeko@mac.com>
2026-01-27 16:30:54 +0900

    Update keymap.c

--- XYZDP/keymap.c
+++ XYZDP/keymap.c
@@ -1312,16 +1312,24 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     case L_Number_ANSI:
     case L_Cursor:
     case L_Cursor_ANSI:
-    case L_LeftPinky:
-    case L_RightPinky:
       status_led(0b1011, led_pattern_off);
       status_led(0b0100, led_pattern_on);
+      break;
+    case L_LeftPinky:
+    case L_RightPinky:
+      status_led(0b1111, led_pattern_off);
       break;
     case L_LeftPinkyThumb:
     case L_RightPinkyThumb:
     case L_BothThumb:
     case L_BothThumb_ANSI:
+      status_led(0b1000, led_pattern_off);
+      status_led(0b0111, led_pattern_on);
+      break;
     case L_BothPinky:
+      status_led(0b1100, led_pattern_off);
+      status_led(0b0011, led_pattern_on);
+      break;    
     case L_BothPinkyThumb:
       status_led(0b1000, led_pattern_off);
       status_led(0b0111, led_pattern_on);
