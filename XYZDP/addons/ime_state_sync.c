72b08645b1819fd5349e42cce55c6b92c0004bf6
kokeko-kokeko <kokeko@mac.com>
2026-01-27 11:34:11 +0900

    Update ime_state_sync.c

--- XYZDP/addons/ime_state_sync.c
+++ XYZDP/addons/ime_state_sync.c
@@ -146,42 +146,54 @@ void housekeeping_task_ime_state_sync(void) {
 }
 
 void set_layer_color_overlay_ime_state_sync(void) {
-  // use golden angle 255 * phi
-  const uint8_t h_diff = 97;
-  
-  HSV hsv = rgb_matrix_get_hsv();
-  hsv.h -= g_rgb_timer >> 4;
-  hsv.s = 255;
-  
-  // copy logic from breathing_anim.h 
-  // use different speed divide
-  uint8_t speed = rgb_matrix_get_speed(); 
-  uint16_t time = scale16by8(g_rgb_timer, speed / 3);
-  hsv.v = scale8(abs8(sin8(time) - 128) * 2, hsv.v);
-
   // IME state sync syntem state
   if (ime_on) {
-    hsv.h -= h_diff;
-    RGB rgb = hsv_to_rgb(hsv);
-    rgb_matrix_set_color(44, rgb.r, rgb.g, rgb.b);
+    rgb_matrix_set_color(44, 0, 0, 0);
     
     if (ime_kk) {
-      rgb_matrix_set_color(38, rgb.r, rgb.g, rgb.b);
-    } else {
       rgb_matrix_set_color(38, 0, 0, 0);
     }
     
     if (iss_sync) {
-      rgb_matrix_set_color(23, rgb.r, rgb.g, rgb.b);
-    } else {
       rgb_matrix_set_color(23, 0, 0, 0);
     }
-    rgb_matrix_set_color(5, 0, 0, 0);
-    rgb_matrix_set_color(11, 0, 0, 0);
-    rgb_matrix_set_color(17, 0, 0, 0);
-    rgb_matrix_set_color(26, 0, 0, 0);
-    rgb_matrix_set_color(32, 0, 0, 0);
   }
+  // use golden angle 255 * phi
+  //const uint8_t h_diff = 97;
+  
+  //HSV hsv = rgb_matrix_get_hsv();
+  //hsv.h -= g_rgb_timer >> 4;
+  //hsv.s = 255;
+  
+  // copy logic from breathing_anim.h 
+  // use different speed divide
+  //uint8_t speed = rgb_matrix_get_speed(); 
+  //uint16_t time = scale16by8(g_rgb_timer, speed / 3);
+  //hsv.v = scale8(abs8(sin8(time) - 128) * 2, hsv.v);
+
+  // IME state sync syntem state
+  //if (ime_on) {
+  //  hsv.h -= h_diff;
+  //  RGB rgb = hsv_to_rgb(hsv);
+  //  rgb_matrix_set_color(44, rgb.r, rgb.g, rgb.b);
+    
+  //  if (ime_kk) {
+  //    rgb_matrix_set_color(38, rgb.r, rgb.g, rgb.b);
+  //  } else {
+  //    rgb_matrix_set_color(38, 0, 0, 0);
+  //  }
+    
+  //  if (iss_sync) {
+  //    rgb_matrix_set_color(23, rgb.r, rgb.g, rgb.b);
+  //  } else {
+  //    rgb_matrix_set_color(23, 0, 0, 0);
+  //  }
+  //  rgb_matrix_set_color(5, 0, 0, 0);
+  //  rgb_matrix_set_color(11, 0, 0, 0);
+  //  rgb_matrix_set_color(17, 0, 0, 0);
+  //  rgb_matrix_set_color(26, 0, 0, 0);
+  //  rgb_matrix_set_color(32, 0, 0, 0);
+  //}
 }
 
 void set_layer_color_firmware_map_ime_state_sync(void) {
