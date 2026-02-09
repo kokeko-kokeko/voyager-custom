b03d53550ff32995132aabfad866b5a98fc48072
kokeko-kokeko <kokeko@mac.com>
2026-02-09 14:43:37 +0900

    Update ime_state_sync.h

--- XYZDP/addons/ime_state_sync.h
+++ XYZDP/addons/ime_state_sync.h
@@ -10,5 +10,3 @@ void post_process_record_ime_state_sync(uint16_t keycode, keyrecord_t *record);
 void housekeeping_task_ime_state_sync(void);
 
 void set_layer_color_overlay_ime_state_sync(void);
-
-void set_layer_color_firmware_map_ime_state_sync(void);
