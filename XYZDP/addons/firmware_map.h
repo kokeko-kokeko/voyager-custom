0a91261b7d0e0cec6cb7a087fd7696a8ec08a83a
kokeko-kokeko <kokeko@mac.com>
2026-02-09 14:01:47 +0900

    Update firmware_map.h

--- XYZDP/addons/firmware_map.h
+++ XYZDP/addons/firmware_map.h
@@ -1,5 +1,9 @@
 #pragma once
 
+void firmware_map_keyrecord(const keyrecord_t * const record);
+
+void halt_map_keyrecord(const keyrecord_t * const record);
+
 void set_layer_color_firmware_map(void);
 
 void set_layer_color_halt_map(void);
