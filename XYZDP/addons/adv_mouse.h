f85ed0fab3ae4fe45b8c896314625d1409699f37
kokeko-kokeko <kokeko@mac.com>
2026-02-09 11:30:00 +0900

    Update adv_mouse.h

--- XYZDP/addons/adv_mouse.h
+++ XYZDP/addons/adv_mouse.h
@@ -13,4 +13,4 @@ report_mouse_t pointing_device_task_adv_mouse(report_mouse_t mouse_report);
 layer_state_t layer_state_set_adv_mouse(const layer_state_t state);
 void housekeeping_task_adv_mouse(void);
 
-void set_layer_color_mouse_map(void);
+void set_layer_color_overlay_mouse(void);
