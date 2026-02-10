2b79dbb413491eb214347a74ad0bcc493af0c645
kokeko-kokeko <kokeko@mac.com>
2026-02-10 17:19:17 +0900

    Update fade_matrix.h

--- XYZDP/addons/fade_matrix.h
+++ XYZDP/addons/fade_matrix.h
@@ -2,10 +2,6 @@
 
 uint8_t get_pos_from_keyrecord(const keyrecord_t * const record);
 
-void fade_matrix_set_mode(const uint8_t mode);
-void fade_matrix_load_preset(void);
-void fade_matrix_load_preset_powersave(void);
-
 bool fade_matrix_rgb_sld_keyrecord(const keyrecord_t * const record);
 bool fade_matrix_set_hue_keyrecord(const keyrecord_t * const record);
 bool fade_matrix_set_sat_keyrecord(const keyrecord_t * const record);
