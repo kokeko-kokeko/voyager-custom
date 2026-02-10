13ca8e663c279f73e062602ce0aef4a093fd3dc7
kokeko-kokeko <kokeko@mac.com>
2026-02-10 16:21:32 +0900

    Update status_led.h

--- XYZDP/addons/status_led.h
+++ XYZDP/addons/status_led.h
@@ -8,4 +8,5 @@ extern const uint8_t led_pattern_oneshot[];
 
 void status_led(const uint8_t mask, const uint8_t * const pattern);
 
+void keyboard_post_init_status_led(void);
 void housekeeping_task_status_led(void);
