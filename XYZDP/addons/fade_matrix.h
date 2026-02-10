50c5cab0f21200b97582371a8c1b7825f43fc449
kokeko-kokeko <kokeko@mac.com>
2026-02-10 10:41:05 +0900

    Update fade_matrix.h

--- XYZDP/addons/fade_matrix.h
+++ XYZDP/addons/fade_matrix.h
@@ -8,23 +8,11 @@ void fade_matrix_set_sat_keyrecord(const keyrecord_t * const record);
 void fade_matrix_set_val_keyrecord(const keyrecord_t * const record);
 void fade_matrix_set_speed_keyrecord(const keyrecord_t * const record);
 
-//void fade_matrix_increase_hue(void);
-//void fade_matrix_decrease_hue(void);
-//void fade_matrix_increase_sat(void);
-//void fade_matrix_decrease_sat(void);
-//void fade_matrix_increase_val(void);
-//void fade_matrix_decrease_val(void);
-//void fade_matrix_increase_speed(void);
-//void fade_matrix_decrease_speed(void);
-//void fade_matrix_toggle(void);
-//void fade_matrix_step(void);
-
 void fade_matrix_load_preset(void);
 void fade_matrix_load_preset_powersave(void);
 
 void keyboard_post_init_fade_matrix(void);
 bool process_record_fade_matrix(uint16_t keycode, keyrecord_t *record);
-void post_process_record_fade_matrix(uint16_t keycode, keyrecord_t *record);
 report_mouse_t pointing_device_task_fade_matrix(report_mouse_t mouse_report);
 layer_state_t layer_state_set_fade_matrix(layer_state_t state);
 void housekeeping_task_fade_matrix(void);
