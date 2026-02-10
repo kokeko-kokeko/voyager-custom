9f33da455843a6438eef25eb296183470632d767
kokeko-kokeko <kokeko@mac.com>
2026-02-10 17:13:11 +0900

    Update keymap.c

--- XYZDP/keymap.c
+++ XYZDP/keymap.c
@@ -726,26 +726,10 @@ static bool process_record_extra_custom_keycodes(uint16_t keycode, keyrecord_t *
   if (keycode == HSV_86_255_255) return firmware_map_exit_all_keyrecord(record);
 
   // preset
-  if (keycode == HSV_172_255_210) {
-    if (record->event.pressed) {
-      fade_matrix_load_preset();
-      status_led(0b0101, led_pattern_oneshot);
-    }
-    
-    return false;  
-  }
-
-  if (keycode == HSV_172_255_211) return false;  
-  if (keycode == HSV_172_255_212) return false;  
-  
-  if (keycode == HSV_172_255_213) {
-    if (record->event.pressed) {
-      fade_matrix_load_preset_powersave();
-      status_led(0b1010, led_pattern_oneshot);
-    }
-    
-    return false;
-  }
+  if (keycode == HSV_172_255_210) return fade_matrix_load_preset_keyrecord(record);
+  if (keycode == HSV_172_255_211) return false;
+  if (keycode == HSV_172_255_212) return false;
+  if (keycode == HSV_172_255_213) return fade_matrix_load_preset_powersave_keyrecord(record);
 
   // mouse
   if (keycode == NAVIGATOR_TURBO) return false;
