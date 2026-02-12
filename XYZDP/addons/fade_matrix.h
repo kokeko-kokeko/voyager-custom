2e6ee739d93a4ef56490bd8ee76367ac91df28b8
kokeko-kokeko <kokeko@mac.com>
2026-02-12 17:30:56 +0900

    Update fade_matrix.h

--- XYZDP/addons/fade_matrix.h
+++ XYZDP/addons/fade_matrix.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#define FADE_MATRIX_POS_SIZE 52
+#define FADE_MATRIX_INDEX_SIZE 48
+
 uint8_t get_pos_from_keyrecord(const keyrecord_t * const record);
 
 bool fade_matrix_rgb_sld_keyrecord(const keyrecord_t * const record);
