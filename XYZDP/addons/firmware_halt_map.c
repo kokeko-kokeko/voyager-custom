2e5ff2a3990b5de8047236dbdebb9dfacdf8f0a1
kokeko-kokeko <kokeko@mac.com>
2026-02-18 18:49:53 +0900

    Update firmware_halt_map.c

--- XYZDP/addons/firmware_halt_map.c
+++ XYZDP/addons/firmware_halt_map.c
@@ -393,8 +393,11 @@ bool halt_map_main_keyrecord(const keyrecord_t * const record) {
       
   if (pos == POSITION_Halt) {
     if (record->event.pressed) {
+      // press
       rgb_matrix_disable_noeeprom();
+      clear_keyboard();      
     } else {
+      // telease
       clear_keyboard();
       wait_ms(10);
       clear_keyboard();
