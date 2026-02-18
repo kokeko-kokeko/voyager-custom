83d4df827fa3892d85c3d6aed55567363313045e
kokeko-kokeko <kokeko@mac.com>
2026-02-18 20:11:00 +0900

    Update ime_state_sync.c

--- XYZDP/addons/ime_state_sync.c
+++ XYZDP/addons/ime_state_sync.c
@@ -65,8 +65,8 @@ bool process_record_ime_state_sync(uint16_t keycode, keyrecord_t *record) {
     case LT(LAYER_Function, KC_LANGUAGE_1):
       if (record->tap.count > 0) {
         if (record->event.pressed) {
-          if (layer_state_is(LAYER_OneShot) | layer_state_is(LAYER_Function) | layer_state_is(LAYER_Number) | 
-              layer_state_is(LAYER_Cursor) | layer_state_is(LAYER_R_pinky) | layer_state_is(LAYER_L_pinky)) {
+          if (layer_state_is(LAYER_Function) | layer_state_is(LAYER_Number) | layer_state_is(LAYER_Cursor) |
+              layer_state_is(LAYER_R_pinky) | layer_state_is(LAYER_L_pinky)) {
             // reverse side (upper layer)
             if ((get_mods() & MOD_MASK_CSAG) == 0) {
               ime_on = false;
@@ -91,8 +91,8 @@ bool process_record_ime_state_sync(uint16_t keycode, keyrecord_t *record) {
     case LT(LAYER_OneShot, KC_LANGUAGE_2):
       if (record->tap.count > 0) {
         if (record->event.pressed) {
-          if (layer_state_is(LAYER_OneShot) | layer_state_is(LAYER_Function) | layer_state_is(LAYER_Number) |
-              layer_state_is(LAYER_Cursor) | layer_state_is(LAYER_R_pinky) | layer_state_is(LAYER_L_pinky)) {
+          if (layer_state_is(LAYER_Function) | layer_state_is(LAYER_Number) | layer_state_is(LAYER_Cursor) |
+              layer_state_is(LAYER_R_pinky) | layer_state_is(LAYER_L_pinky)) {
             // reverse side (upper layer)
             if ((get_mods() & MOD_MASK_CAG) == 0) {
               ime_on = true;
