9a3cf006413f8da6027c186929c2f0745e30936e
kokeko-kokeko <kokeko@mac.com>
2026-02-21 14:23:56 +0900

    Update fade_matrix.h

--- XYZDP/addons/fade_matrix.h
+++ XYZDP/addons/fade_matrix.h
@@ -6,12 +6,19 @@
 uint8_t get_pos_from_keyrecord(const keyrecord_t * const record);
 
 bool fade_matrix_rgb_sld_keyrecord(const keyrecord_t * const record);
-bool fade_matrix_set_hue_keyrecord(const keyrecord_t * const record);
-bool fade_matrix_set_sat_keyrecord(const keyrecord_t * const record);
-bool fade_matrix_set_val_keyrecord(const keyrecord_t * const record);
-bool fade_matrix_set_speed_keyrecord(const keyrecord_t * const record);
-bool fade_matrix_load_default_keyrecord(const keyrecord_t * const record);
-bool fade_matrix_load_powersave_keyrecord(const keyrecord_t * const record);
+
+//bool fade_matrix_set_hue_keyrecord(const keyrecord_t * const record);
+//bool fade_matrix_set_sat_keyrecord(const keyrecord_t * const record);
+//bool fade_matrix_set_val_keyrecord(const keyrecord_t * const record);
+//bool fade_matrix_set_speed_keyrecord(const keyrecord_t * const record);
+bool fade_matrix_set_color_palette_keyrecord(const keyrecord_t * const record);
+
+//bool fade_matrix_load_default_keyrecord(const keyrecord_t * const record);
+//bool fade_matrix_load_powersave_keyrecord(const keyrecord_t * const record);
+bool fade_matrix_load_color_palette_keyrecord(const keyrecord_t * const record);
+
+bool fade_matrix_color_palette_sat_keyrecord(const keyrecord_t * const record);
+bool fade_matrix_color_palette_val_keyrecord(const keyrecord_t * const record);
 
 void keyboard_post_init_fade_matrix(void);
 bool pre_process_record_fade_matrix(uint16_t keycode, keyrecord_t *record);
@@ -21,7 +28,9 @@ report_mouse_t pointing_device_task_fade_matrix(report_mouse_t mouse_report);
 layer_state_t layer_state_set_fade_matrix(layer_state_t state);
 void housekeeping_task_fade_matrix(void);
 
-void set_layer_color_hue_map(void);
-void set_layer_color_sat_map(void);
-void set_layer_color_val_map(void);
-void set_layer_color_speed_map(void);
+//void set_layer_color_hue_map(void);
+//void set_layer_color_sat_map(void);
+//void set_layer_color_val_map(void);
+//void set_layer_color_speed_map(void);
+void set_layer_color_palette_map(void);
+
