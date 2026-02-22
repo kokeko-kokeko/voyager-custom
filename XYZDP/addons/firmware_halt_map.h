3bb1eccae1432ada2d623a6bda047fcffc361af7
kokeko-kokeko <kokeko@mac.com>
2026-02-22 17:08:39 +0900

    Update firmware_halt_map.h

--- XYZDP/addons/firmware_halt_map.h
+++ XYZDP/addons/firmware_halt_map.h
@@ -2,11 +2,12 @@
 
 bool firmware_map_main_keyrecord(const keyrecord_t * const record);
 
-void set_layer_color_firmware_map(void);
-
 bool firmware_map_enter_color_palette_keyrecord(const keyrecord_t * const record);
 bool firmware_map_exit_all_keyrecord(const keyrecord_t * const record);
 
 bool firmware_map_invoke_halt_keyrecord(const keyrecord_t * const record);
 
 void housekeeping_task_exec_halt(void);
+
+void set_layer_color_firmware_map(void);
+
