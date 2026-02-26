cefcf467507f3e992066202a1c63e938d5578c40
kokeko-kokeko <kokeko@mac.com>
2026-02-26 18:06:03 +0900

    Update firmware_map.c

--- XYZDP/addons/firmware_map.c
+++ XYZDP/addons/firmware_map.c
@@ -385,7 +385,7 @@ bool firmware_map_invoke_halt_keyrecord(const keyrecord_t * const record) {
     halt_request0 = true;
     halt_request1 = true;
     halt_request2 = true;
-    exec_halt_trigger = now + 31;
+    exec_halt_trigger = now + 23;
     
     rgb_matrix_mode_noeeprom(RGB_MATRIX_NONE);
     rgb_matrix_disable_noeeprom();
