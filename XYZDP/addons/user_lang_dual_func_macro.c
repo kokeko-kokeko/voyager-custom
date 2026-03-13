56f68fc75b1cf718f39f13711ef73fdbbc766533
kokeko-kokeko <kokeko@mac.com>
2026-03-13 11:13:19 +0900

    Update user_lang_dual_func_macro.c

--- XYZDP/addons/user_lang_dual_func_macro.c
+++ XYZDP/addons/user_lang_dual_func_macro.c
@@ -1,1 +1,8 @@
+#include QMK_KEYBOARD_H
+#include "version.h"
+#define MOON_LED_LEVEL LED_LEVEL
+#ifndef ZSA_SAFE_RANGE
+#define ZSA_SAFE_RANGE SAFE_RANGE
+#endif
 
+#include "addons/user_lang_dual_func_macro.h"
