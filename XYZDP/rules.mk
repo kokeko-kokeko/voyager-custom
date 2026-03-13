c8622d23db40d48ca4fcdf316567dfe4afc9f4b7
kokeko-kokeko <kokeko@mac.com>
2026-03-13 18:24:04 +0900

    Update rules.mk

--- XYZDP/rules.mk
+++ XYZDP/rules.mk
@@ -29,7 +29,7 @@ SRC += addons/firmware_map.c
 SRC += addons/ime_state_sync.c
 SRC += addons/layer_mod_overlay.c
 SRC += addons/status_led.c
-SRC += addons/user_lang_dual_func_macro.c
+SRC += addons/user_dual_func_macro_lang.c
 
 LTO_ENABLE = yes
 EXTRAFLAGS += -flto=auto -O3
