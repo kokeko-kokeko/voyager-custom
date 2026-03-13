f007b3ab127046ac93e6a4d5e27d06c73f7e56a2
kokeko-kokeko <kokeko@mac.com>
2026-03-13 11:02:00 +0900

    Update keymap.c

--- XYZDP/keymap.c
+++ XYZDP/keymap.c
@@ -674,7 +674,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     
     // Firmware  
     if (keycode == HSV_0_255_241) return firmware_map_main_keyrecord(record);
-    if (keycode == MT(MOD_LGUI | MOD_LSFT | MOD_LCTL, KC_B)) return firmware_map_invoke_halt_keyrecord(record);
+    if (keycode == KC_B) return firmware_map_invoke_halt_keyrecord(record);
     
     // Color Palette
     if (keycode == HSV_0_255_242) return fade_matrix_color_palette_main_keyrecord(record);
@@ -685,7 +685,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (keycode == HSV_172_255_242) return fade_matrix_color_palette_load_preset_keyrecord(record);
     
     // layer move
-    if (keycode == KC_B) return firmware_map_enter_color_palette_keyrecord(record);
+    if (keycode == MT(MOD_LGUI | MOD_LSFT | MOD_LCTL, KC_A)) return firmware_map_enter_color_palette_keyrecord(record);
     if (keycode == HSV_86_255_242) return firmware_map_exit_all_keyrecord(record);
   }
   
