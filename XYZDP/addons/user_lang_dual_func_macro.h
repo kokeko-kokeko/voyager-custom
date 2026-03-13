1aee39315a66b4e14d2d14feb3d5ec2ed8988e5f
kokeko-kokeko <kokeko@mac.com>
2026-03-13 11:37:07 +0900

    Update user_lang_dual_func_macro.h

--- XYZDP/addons/user_lang_dual_func_macro.h
+++ XYZDP/addons/user_lang_dual_func_macro.h
@@ -2,10 +2,13 @@
 
 // use MT with 3 mod for dual func without Meh Hyper
 #define MOD_UDFN1 (MOD_RCTL | MOD_RSFT | MOD_RALT)
+
 #define MOD_UDFN2 (MOD_LCTL | MOD_LSFT | MOD_LGUI)
 #define MOD_UDFN3 (MOD_RCTL | MOD_RSFT | MOD_RGUI)
+
 #define MOD_UDFN4 (MOD_LCTL | MOD_LALT | MOD_LGUI)
 #define MOD_UDFN5 (MOD_RCTL | MOD_RALT | MOD_RGUI)
+
 #define MOD_UDFN6 (MOD_LSFT | MOD_LALT | MOD_LGUI)
 #define MOD_UDFN7 (MOD_RSFT | MOD_RALT | MOD_RGUI)
 
