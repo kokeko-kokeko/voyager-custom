7a06733e9150fdbfb4287973ac19d821da23469f
kokeko-kokeko <kokeko@mac.com>
2026-03-18 12:45:50 +0900

    Update user_dual_func_macro_lang.h

--- XYZDP/addons/user_dual_func_macro_lang.h
+++ XYZDP/addons/user_dual_func_macro_lang.h
@@ -1,22 +1,21 @@
 #pragma once
 
-// use MT with 3,4 mod for dual func without Meh Hyper, more safe
+// use MT with 3,4 mod for dual func include Meh Hyper, if use passthrough
 
-#define MOD_UDFN1 (MOD_LCTL | MOD_LSFT | MOD_LGUI)
-#define MOD_UDFN2 (MOD_RCTL | MOD_RSFT | MOD_RGUI)
+#define MOD_UDFN1 (MOD_LCTL | MOD_LSFT | MOD_LALT)
+#define MOD_UDFN2 (MOD_RCTL | MOD_RSFT | MOD_RALT)
 
-#define MOD_UDFN3 (MOD_LCTL | MOD_LALT | MOD_LGUI)
-#define MOD_UDFN4 (MOD_RCTL | MOD_RALT | MOD_RGUI)
+#define MOD_UDFN3 (MOD_LCTL | MOD_LSFT | MOD_LGUI)
+#define MOD_UDFN4 (MOD_RCTL | MOD_RSFT | MOD_RGUI)
 
-#define MOD_UDFN5 (MOD_LSFT | MOD_LALT | MOD_LGUI)
-#define MOD_UDFN6 (MOD_RSFT | MOD_RALT | MOD_RGUI)
+#define MOD_UDFN5 (MOD_LCTL | MOD_LALT | MOD_LGUI)
+#define MOD_UDFN6 (MOD_RCTL | MOD_RALT | MOD_RGUI)
 
-// use 7 & 8 R Meh Hyper
-// #define MOD_MEH (MOD_LCTL | MOD_LSFT | MOD_LALT)
-// #define MOD_HYPR (MOD_LCTL | MOD_LSFT | MOD_LALT | MOD_LGUI)
+#define MOD_UDFN7 (MOD_LSFT | MOD_LALT | MOD_LGUI)
+#define MOD_UDFN8 (MOD_RSFT | MOD_RALT | MOD_RGUI)
 
-#define MOD_UDFN7 (MOD_RCTL | MOD_RSFT | MOD_RALT)
-#define MOD_UDFN8 (MOD_RCTL | MOD_RSFT | MOD_RALT | MOD_RGUI)
+#define MOD_UDFN9 (MOD_LCTL | MOD_LSFT | MOD_LALT | MOD_LGUI)
+#define MOD_UDFN10 (MOD_RCTL | MOD_RSFT | MOD_RALT | MOD_RGUI)
 
 //#define MT(mod, kc) (QK_MOD_TAP | (((mod)&0x1F) << 8) | ((kc)&0xFF))
 #define UDFN1(kc) MT(MOD_UDFN1, kc)
@@ -27,6 +26,8 @@
 #define UDFN6(kc) MT(MOD_UDFN6, kc)
 #define UDFN7(kc) MT(MOD_UDFN7, kc)
 #define UDFN8(kc) MT(MOD_UDFN8, kc)
+#define UDFN9(kc) MT(MOD_UDFN8, kc)
+#define UDFN10(kc) MT(MOD_UDFN8, kc)
 
 void jis_enable(void);
 void jis_disable(void);
