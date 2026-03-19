ed3cc64450cfba61bd5443531369d861035bdcb6
kokeko-kokeko <kokeko@mac.com>
2026-03-19 16:44:42 +0900

    Update orthogonal_mod_layer_lang.h

--- XYZDP/addons/orthogonal_mod_layer_lang.h
+++ XYZDP/addons/orthogonal_mod_layer_lang.h
@@ -6,6 +6,17 @@
 // THORnS : Tap Hold OverRide, both override, fix shift
 // MCFW : MaCro FirmWare
 
+#define MOD_HOOR (MOD_LCTL)
+
+#define MOD_THOR1 (MOD_RCTL)
+#define MOD_THOR1S (MOD_RCTL | MOD_RSFT)
+
+#define MOD_THOR2 (MOD_LALT)
+#define MOD_THOR2S (MOD_LALT | MOD_LSFT)
+
+#define MOD_MCFW (MOD_LCTL | MOD_LSFT)
+
+
 #define MOD_UDFN1 (MOD_LCTL | MOD_LSFT | MOD_LALT)
 #define MOD_UDFN2 (MOD_RCTL | MOD_RSFT | MOD_RALT)
 
@@ -22,6 +33,17 @@
 #define MOD_UDFN10 (MOD_RCTL | MOD_RSFT | MOD_RALT | MOD_RGUI)
 
 //#define MT(mod, kc) (QK_MOD_TAP | (((mod)&0x1F) << 8) | ((kc)&0xFF))
+#define HOOR(kc) MT(MOD_HOOR, kc)
+
+#define THOR1(kc) MT(MOD_THOR1, kc)
+#define THOR1S(kc) MT(MOD_THOR1S, kc)
+
+#define THOR2(kc) MT(MOD_THOR1, kc)
+#define THOR2S(kc) MT(MOD_THOR1S, kc)
+
+#define MCFW(kc) MT(MOD_MCFW, kc)
+
+
 #define UDFN1(kc) MT(MOD_UDFN1, kc)
 #define UDFN2(kc) MT(MOD_UDFN2, kc)
 #define UDFN3(kc) MT(MOD_UDFN3, kc)
