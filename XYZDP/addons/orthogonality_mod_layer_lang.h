e563b3d2ca427a5c90563d3936ba25a426e4d488
kokeko-kokeko <kokeko@mac.com>
2026-03-20 04:14:11 +0900

    Update orthogonality_mod_layer_lang.h

--- XYZDP/addons/orthogonality_mod_layer_lang.h
+++ XYZDP/addons/orthogonality_mod_layer_lang.h
@@ -8,24 +8,24 @@
 
 #define MOD_HOOR (MOD_LCTL)
 
+#define MOD_MCFW (MOD_LCTL | MOD_LSFT)
+
 #define MOD_THOR1 (MOD_RCTL)
 #define MOD_THOR1S (MOD_RCTL | MOD_RSFT)
 
 #define MOD_THOR2 (MOD_LALT)
 #define MOD_THOR2S (MOD_LALT | MOD_LSFT)
 
-#define MOD_MCFW (MOD_LCTL | MOD_LSFT)
-
 //#define MT(mod, kc) (QK_MOD_TAP | (((mod)&0x1F) << 8) | ((kc)&0xFF))
 #define HOOR(kc) MT(MOD_HOOR, kc)
+
+#define MCFW(kc) MT(MOD_MCFW, kc)
 
 #define THOR1(kc) MT(MOD_THOR1, kc)
 #define THOR1S(kc) MT(MOD_THOR1S, kc)
 
 #define THOR2(kc) MT(MOD_THOR2, kc)
 #define THOR2S(kc) MT(MOD_THOR2S, kc)
-
-#define MCFW(kc) MT(MOD_MCFW, kc)
 
 void jis_enable(void);
 void jis_disable(void);
