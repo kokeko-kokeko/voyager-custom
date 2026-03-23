82eb1036e0b0a117636738faf9001eafbfd58495
kokeko-kokeko <kokeko@mac.com>
2026-03-23 15:46:57 +0900

    Update keymap.c

--- XYZDP/keymap.c
+++ XYZDP/keymap.c
@@ -280,6 +280,7 @@ void keyboard_post_init_user(void) {
 
 bool process_detected_host_os_user(os_variant_t detected_os) {
   if (process_detected_host_os_fade_matrix(detected_os) == false) return false;
+  if (process_detected_host_os_ortho_hold_os_locale_aware(detected_os) == false) return false;
 
   return true;
 }
