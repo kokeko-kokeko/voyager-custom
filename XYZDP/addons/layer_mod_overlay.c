7b3641fd8a000cd513391875ab04fb5ae54948de
kokeko-kokeko <kokeko@mac.com>
2026-03-24 16:08:00 +0900

    Update layer_mod_overlay.c

--- XYZDP/addons/layer_mod_overlay.c
+++ XYZDP/addons/layer_mod_overlay.c
@@ -210,34 +210,42 @@ void set_layer_color_overlay_mod(void) {
   
   if (get_mods() & MOD_BIT_LCTRL) {
     rgb_matrix_set_color(10, 0, 0, 0);
+    rgb_matrix_set_color(16, 0, 0, 0);
   }
 
   if (get_mods() & MOD_BIT_LSHIFT) {
     rgb_matrix_set_color(9, 0, 0, 0);
+    rgb_matrix_set_color(15, 0, 0, 0);
   }
 
   if (get_mods() & MOD_BIT_LALT) {
     rgb_matrix_set_color(8, 0, 0, 0);
+    rgb_matrix_set_color(14, 0, 0, 0);
   }
 
   if (get_mods() & MOD_BIT_LGUI) {
     rgb_matrix_set_color(7, 0, 0, 0);
+    rgb_matrix_set_color(13, 0, 0, 0);
   }  
 
   if (get_mods() & MOD_BIT_RCTRL) {
     rgb_matrix_set_color(33, 0, 0, 0);
+    rgb_matrix_set_color(39, 0, 0, 0);
   }
 
   if (get_mods() & MOD_BIT_RSHIFT) {
     rgb_matrix_set_color(34, 0, 0, 0);
+    rgb_matrix_set_color(40, 0, 0, 0);
   }
 
   if (get_mods() & MOD_BIT_RALT) {
     rgb_matrix_set_color(35, 0, 0, 0);
+    rgb_matrix_set_color(41, 0, 0, 0);
   }
 
   if (get_mods() & MOD_BIT_RGUI) {
     rgb_matrix_set_color(36, 0, 0, 0);
+    rgb_matrix_set_color(42, 0, 0, 0);
   }
 
   // use golden angle 255 * phi
