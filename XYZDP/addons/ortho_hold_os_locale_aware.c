43086439cd7e863a0e3902cb8b9f989baef46c5d
kokeko-kokeko <kokeko@mac.com>
2026-03-24 11:40:58 +0900

    Update ortho_hold_os_locale_aware.c

--- XYZDP/addons/ortho_hold_os_locale_aware.c
+++ XYZDP/addons/ortho_hold_os_locale_aware.c
@@ -32,45 +32,45 @@ _Static_assert(sizeof(hold_action_t) <= 4, "Hold action struct too large!!");
 static hold_action_t conv_pos_to_mods(const uint8_t pos) {
   switch (pos) {
     case  3: return (hold_action_t){MOD_BIT_LALT | MOD_BIT_LSHIFT, 0, false, false};
-    case  4: return {MOD_BIT_LALT | MOD_BIT_LCTRL, 0, false, false};
-    case  5: return {MOD_BIT_LGUI | MOD_BIT_LALT | MOD_BIT_LSHIFT | MOD_BIT_LCTRL, 0, false, false};
+    case  4: return (hold_action_t){MOD_BIT_LALT | MOD_BIT_LCTRL, 0, false, false};
+    case  5: return (hold_action_t){MOD_BIT_LGUI | MOD_BIT_LALT | MOD_BIT_LSHIFT | MOD_BIT_LCTRL, 0, false, false};
 
-    case 28: return {MOD_BIT_RALT | MOD_BIT_RSHIFT, 0, false, false};
-    case 27: return {MOD_BIT_RALT | MOD_BIT_RCTRL, 0, false, false};
-    case 26: return {MOD_BIT_LALT | MOD_BIT_LSHIFT | MOD_BIT_LCTRL, 0, false, false};
+    case 28: return (hold_action_t){MOD_BIT_RALT | MOD_BIT_RSHIFT, 0, false, false};
+    case 27: return (hold_action_t){MOD_BIT_RALT | MOD_BIT_RCTRL, 0, false, false};
+    case 26: return (hold_action_t){MOD_BIT_LALT | MOD_BIT_LSHIFT | MOD_BIT_LCTRL, 0, false, false};
 
-    case  7: return {MOD_BIT_LSHIFT | MOD_BIT_LCTRL, 0, false, false};
-    case  8: return {MOD_BIT_LGUI | MOD_BIT_LALT, 0, false, false};
-    case  9: return {MOD_BIT_LSHIFT, 0, false, false};
-    case 10: return {MOD_BIT_LCTRL, 0, false, false};
-    case 11: return {MOD_BIT_LGUI | MOD_BIT_LSHIFT | MOD_BIT_LCTRL, 0, false, false};
+    case  7: return (hold_action_t){MOD_BIT_LSHIFT | MOD_BIT_LCTRL, 0, false, false};
+    case  8: return (hold_action_t){MOD_BIT_LGUI | MOD_BIT_LALT, 0, false, false};
+    case  9: return (hold_action_t){MOD_BIT_LSHIFT, 0, false, false};
+    case 10: return (hold_action_t){MOD_BIT_LCTRL, 0, false, false};
+    case 11: return (hold_action_t){MOD_BIT_LGUI | MOD_BIT_LSHIFT | MOD_BIT_LCTRL, 0, false, false};
 
-    case 36: return {MOD_BIT_RSHIFT | MOD_BIT_RCTRL, 0, false, false};
-    case 35: return {MOD_BIT_RGUI | MOD_BIT_RALT, 0, false, false};
-    case 34: return {MOD_BIT_RSHIFT, 0, false, false};
-    case 33: return {MOD_BIT_RCTRL, 0, false, false};
-    case 32: return {MOD_BIT_RGUI | MOD_BIT_RSHIFT | MOD_BIT_RCTRL, 0, false, false};
+    case 36: return (hold_action_t){MOD_BIT_RSHIFT | MOD_BIT_RCTRL, 0, false, false};
+    case 35: return (hold_action_t){MOD_BIT_RGUI | MOD_BIT_RALT, 0, false, false};
+    case 34: return (hold_action_t){MOD_BIT_RSHIFT, 0, false, false};
+    case 33: return (hold_action_t){MOD_BIT_RCTRL, 0, false, false};
+    case 32: return (hold_action_t){MOD_BIT_RGUI | MOD_BIT_RSHIFT | MOD_BIT_RCTRL, 0, false, false};
 
-    case 12: return {MOD_BIT_LCTRL, 0, false, false};
-    case 13: return {MOD_BIT_LGUI, 0, false, false};
-    case 14: return {MOD_BIT_LALT, 0, false, false};
-    case 15: return {MOD_BIT_LGUI | MOD_BIT_LSHIFT, 0, false, false};
-    case 16: return {MOD_BIT_LGUI | MOD_BIT_LCTRL, 0, false, false};
-    case 17: return {MOD_BIT_LGUI | MOD_BIT_LALT | MOD_BIT_LCTRL, 0, false, false};
+    case 12: return (hold_action_t){MOD_BIT_LCTRL, 0, false, false};
+    case 13: return (hold_action_t){MOD_BIT_LGUI, 0, false, false};
+    case 14: return (hold_action_t){MOD_BIT_LALT, 0, false, false};
+    case 15: return (hold_action_t){MOD_BIT_LGUI | MOD_BIT_LSHIFT, 0, false, false};
+    case 16: return (hold_action_t){MOD_BIT_LGUI | MOD_BIT_LCTRL, 0, false, false};
+    case 17: return (hold_action_t){MOD_BIT_LGUI | MOD_BIT_LALT | MOD_BIT_LCTRL, 0, false, false};
 
-    case 43: return {MOD_BIT_RCTRL, 0, false, false};
-    case 42: return {MOD_BIT_RGUI, 0, false, false};
-    case 41: return {MOD_BIT_RALT, 0, false, false};
-    case 40: return {MOD_BIT_RGUI | MOD_BIT_RSHIFT, 0, false, false};
-    case 39: return {MOD_BIT_RGUI | MOD_BIT_RCTRL, 0, false, false};
-    case 38: return {MOD_BIT_RGUI | MOD_BIT_RALT | MOD_BIT_RCTRL, 0, false, false};
+    case 43: return (hold_action_t){MOD_BIT_RCTRL, 0, false, false};
+    case 42: return (hold_action_t){MOD_BIT_RGUI, 0, false, false};
+    case 41: return (hold_action_t){MOD_BIT_RALT, 0, false, false};
+    case 40: return (hold_action_t){MOD_BIT_RGUI | MOD_BIT_RSHIFT, 0, false, false};
+    case 39: return (hold_action_t){MOD_BIT_RGUI | MOD_BIT_RCTRL, 0, false, false};
+    case 38: return (hold_action_t){MOD_BIT_RGUI | MOD_BIT_RALT | MOD_BIT_RCTRL, 0, false, false};
 
-    case 18: return {MOD_BIT_LSHIFT, 0, false, false};
+    case 18: return (hold_action_t){MOD_BIT_LSHIFT, 0, false, false};
 
-    case 49: return {MOD_BIT_RSHIFT, 0, false, false};
+    case 49: return (hold_action_t){MOD_BIT_RSHIFT, 0, false, false};
   }     
   
-  return 0;
+  return (hold_action_t){0, 0, false, false};
 }
 
 static uint8_t conv_pos_to_layer(const uint8_t pos) {
