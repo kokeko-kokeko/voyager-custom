ed6fe20018bafd56bb44af54f65cf5dc241c9e6a
kokeko-kokeko <kokeko@mac.com>
2026-03-27 21:18:16 +0900

    Update ortho_hold_os_locale_aware.c

--- XYZDP/addons/ortho_hold_os_locale_aware.c
+++ XYZDP/addons/ortho_hold_os_locale_aware.c
@@ -227,10 +227,10 @@ static bool process_record_user_task_switch(const uint16_t keycode, const keyrec
   if ((IS_QK_MOD_TAP(keycode) == false) || (QK_MOD_TAP_GET_MODS(keycode) != MOD_TKSW)) {
     if (is_active) {
       // keep task switch for cursor
-      if (keycode == KC_RIGHT) return true;
-      if (keycode == KC_LEFT) return true;
-      if (keycode == KC_DOWN) return true;
-      if (keycode == KC_UP) return true;
+      if ((keycode == KC_RIGHT) || 
+          (keycode == KC_LEFT) ||
+          (keycode == KC_DOWN) ||
+          (keycode == KC_UP)) return true;
       
       is_active = false;
       unregister_mods(MOD_BIT_RALT);
