8baffb7970dd26f819c5f3256568dfd883b68607
kokeko-kokeko <kokeko@mac.com>
2026-03-31 16:49:00 +0900

    Update fade_matrix.c

--- XYZDP/addons/fade_matrix.c
+++ XYZDP/addons/fade_matrix.c
@@ -17,9 +17,9 @@
 // fade color system
 static fast_timer_t fade_tamrix_trigger = (UINT32_MAX / 2) - 1;
 static const fast_timer_t fade_matrix_activate_delay = 31; // use prime
-static const fast_timer_t fade_matrix_repeat_delay = 11; // use prime
+static const fast_timer_t fade_matrix_repeat_delay = 7; // use prime
 static const fast_timer_t fade_matrix_dimming_delay = 8009; // fixed
-static const fast_timer_t fade_matrix_dimming_repeat_add_delay = 20; // fixed
+static const fast_timer_t fade_matrix_dimming_repeat_add_delay = 24; // fixed
 static fast_timer_t fade_matrix_idle_delay = 30011; // valiable
 
 // system side rgb
