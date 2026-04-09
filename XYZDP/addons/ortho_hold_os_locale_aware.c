61bbd873cfce29dceaae2027946fab465034d439
kokeko-kokeko <kokeko@mac.com>
2026-04-09 20:55:19 +0900

    Update ortho_hold_os_locale_aware.c

--- XYZDP/addons/ortho_hold_os_locale_aware.c
+++ XYZDP/addons/ortho_hold_os_locale_aware.c
@@ -209,9 +209,9 @@ static bool process_record_macro_firmware(const uint16_t keycode, const keyrecor
   // Color Palette
   if (base_code == KC_C) return fade_matrix_color_palette_main_keyrecord(record);
     
-  if (base_code == KC_0) return fade_matrix_color_palette_sel_bit_0_keyrecord(record);
-  if (base_code == KC_1) return fade_matrix_color_palette_sel_bit_1_keyrecord(record);
-  if (base_code == KC_2) return fade_matrix_color_palette_sel_bit_2_keyrecord(record);
+  if (base_code == KC_0) return fade_matrix_color_palette_sel_next_keyrecord(record);
+  if (base_code == KC_1) return false;
+  if (base_code == KC_2) return false;
   
   return true;
 }
