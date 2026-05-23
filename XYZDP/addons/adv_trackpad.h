aeb0eab7afb448f9e32e12ae95920f6d4c865ced
kokeko-kokeko <kokeko@mac.com>
2026-05-23 20:31:48 +0900

    Update adv_trackpad.h

--- XYZDP/addons/adv_trackpad.h
+++ XYZDP/addons/adv_trackpad.h
@@ -1,1 +1,16 @@
+#pragma once
 
+#define AUTO_MOUSE_TIME_SHORT 499
+#define AUTO_MOUSE_TIME_MID 1999
+#define AUTO_MOUSE_TIME_LONG 30011
+
+#define AUTO_MOUSE_DRAG_THRESHOLD 599
+#define AUTO_MOUSE_MULTI_TAP_THRESHOLD 797
+
+void keyboard_post_init_adv_trackpad(void);
+void post_process_record_adv_trackpad(uint16_t keycode, keyrecord_t *record);
+report_mouse_t pointing_device_task_adv_trackpad(report_mouse_t mouse_report);
+layer_state_t layer_state_set_adv_trackpad(layer_state_t state);
+void housekeeping_task_adv_trackpad(void);
+
+void set_layer_color_overlay_trackpad(void);
