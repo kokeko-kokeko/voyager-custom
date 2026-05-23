134759d2d3d2ec6641c8b81598a4149c56d468eb
kokeko-kokeko <kokeko@mac.com>
2026-05-23 20:34:25 +0900

    Update adv_trackpad.c

--- XYZDP/addons/adv_trackpad.c
+++ XYZDP/addons/adv_trackpad.c
@@ -1,1 +1,12 @@
+#include QMK_KEYBOARD_H
+#include "version.h"
+#define MOON_LED_LEVEL LED_LEVEL
+#ifndef ZSA_SAFE_RANGE
+#define ZSA_SAFE_RANGE SAFE_RANGE
+#endif
+
+#include "layer_num.h"
+
+#include "addons/adv_mouse.h"
+#include "addons/status_led.h"
 
