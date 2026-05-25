6d3aaf9d4209f7c76282ca696f9e8e7a534248a4
kokeko-kokeko <kokeko@mac.com>
2026-05-25 11:56:32 +0900

    Update sub_trackball.c

--- XYZDP/addons/sub_trackball.c
+++ XYZDP/addons/sub_trackball.c
@@ -1,1 +1,14 @@
+#include QMK_KEYBOARD_H
+#include "version.h"
+#define MOON_LED_LEVEL LED_LEVEL
+#ifndef ZSA_SAFE_RANGE
+#define ZSA_SAFE_RANGE SAFE_RANGE
+#endif
+
+#include "i2c_master.h"
+#include <stdint.h>
+#include <stdio.h>
+#include "quantum.h"
+
+#include "addons/sub_trackball.h"
 
