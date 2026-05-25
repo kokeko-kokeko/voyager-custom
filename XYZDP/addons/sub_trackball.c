a9e8e554b6836ee0d4113a511ebf578794c035b1
kokeko-kokeko <kokeko@mac.com>
2026-05-25 12:23:42 +0900

    Update sub_trackball.c

--- XYZDP/addons/sub_trackball.c
+++ XYZDP/addons/sub_trackball.c
@@ -12,3 +12,133 @@
 
 #include "addons/sub_trackball.h"
 
+static uint8_t current_cpi = 0;
+static uint8_t new_cpi = NAVIGATOR_TRACKBALL_CPI;
+
+enum layer_num {
+  TB_S_I2C_CONF = 0,
+  TB_S_SPI_CONF,
+  TB_S_SET_CPI,
+  TB_S_QUEUE_MOTION,
+  TB_S_READ_MOTION,
+  TB_S_QUEUE_X_L,
+  TB_S_READ_X_L,
+  TB_S_QUEUE_Y_L,
+  TB_S_READ_Y_L,
+  TB_S_QUEUE_X_H,
+  TB_S_READ_X_H,
+  TB_S_QUEUE_Y_H,
+  TB_S_READ_Y_H,
+  TB_S_SEND_REPORT
+};
+
+static uint8_t tb_state = TB_S_CONF;
+
+static fast_timer_t tb_trigger = 0;
+
+// copy and mod zsa code
+
+// The sequence of commands to configure and boot the paw3805ek sensor.
+paw3805ek_reg_seq_t paw3805ek_configure_seq[] = {
+    {0x06, 0x80},                 // Software reset
+    {0x00, 0x00},                 // Request the sensor ID
+    {0x09 | WRITE_REG_BIT, 0x5A}, // Disable the write protection
+#ifdef MOUSE_EXTENDED_REPORT
+    {0x19 | WRITE_REG_BIT, 0x30}, // Set the sensor orientation, set motion data length to 16 bits
+#else
+    {0x19 | WRITE_REG_BIT, 0x34}, // Set the sensor orientation, set motion data length to 8 bits
+#endif
+    //{0x26 | WRITE_REG_BIT, 0x10}, // Enable burst mode
+    {0x09 | WRITE_REG_BIT, 0x00}, // Enable the write protection
+};
+
+// A wrapper function for i2c_transmit that adds the address of the bridge chip to the data.
+i2c_status_t sci18is606_write(uint8_t *data, uint8_t length) {
+    return i2c_transmit(NAVIGATOR_TRACKBALL_ADDRESS, data, length, NAVIGATOR_TRACKBALL_TIMEOUT);
+}
+
+// A wrapper function for i2c_receive that adds the address of the bridge chip to the data.
+i2c_status_t sci18is606_read(uint8_t *data, uint8_t length) {
+    return i2c_receive(NAVIGATOR_TRACKBALL_ADDRESS, data, length, NAVIGATOR_TRACKBALL_TIMEOUT);
+}
+
+// A wrapper function that allows to write and optionally read from the bridge chip.
+i2c_status_t sci18is606_spi_tx(uint8_t *data, uint8_t length, bool read) {
+    i2c_status_t status = sci18is606_write(data, length);
+    wait_us(length * 15);
+    // Read the SPI response if the command expects it
+    if (read) {
+        status = sci18is606_read(data, length);
+    }
+    //if (status != I2C_STATUS_SUCCESS) {
+    //    trackball_init = 0;
+    //}
+    return status;
+}
+
+// split queue 
+i2c_status_t sci18is606_spi_queue(uint8_t *data, uint8_t length) {
+    return sci18is606_write(data, length);
+}
+
+i2c_status_t sci18is606_spi_read(uint8_t *data, uint8_t length) {
+    return sci18is606_read(data, length);
+}
+
+// Configure the bridge chip to enable SPI mode.
+i2c_status_t sci18is606_configure(void) {
+    uint8_t      spi_conf[2] = {SCI18IS606_CONF_SPI, SCI18IS606_CONF};
+    i2c_status_t status      = sci18is606_write(spi_conf, 2);
+    wait_ms(10);
+    if (status != I2C_STATUS_SUCCESS) {
+        trackball_init = 0;
+    }
+    return status;
+}
+
+bool paw3805ek_set_cpi(void) {
+
+    paw3805ek_reg_seq_t cpi_reg_seq[] = {
+        {0x09 | WRITE_REG_BIT, 0x5A}, // Disable write protection
+        {0x0D | WRITE_REG_BIT, current_cpi},
+        {0x0E | WRITE_REG_BIT, current_cpi},
+        {0x09 | WRITE_REG_BIT, 0x00}, // Enable the write protection
+    };
+
+    // Run the spi sequence to configure the cpi.
+    for (uint8_t i = 0; i < sizeof(cpi_reg_seq) / sizeof(paw3805ek_reg_seq_t); i++) {
+        uint8_t buf[3];
+        buf[0] = NCS_PIN;
+        buf[1] = cpi_reg_seq[i].reg;
+        buf[2] = cpi_reg_seq[i].data;
+        if (sci18is606_spi_tx(buf, 3, true) != I2C_STATUS_SUCCESS) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+// Run the paw3805ek configuration sequence.
+bool paw3805ek_configure(void) {
+    for (uint8_t i = 0; i < sizeof(paw3805ek_configure_seq) / sizeof(paw3805ek_reg_seq_t); i++) {
+        uint8_t buf[3];
+        buf[0] = NCS_PIN;
+        buf[1] = paw3805ek_configure_seq[i].reg;
+        buf[2] = paw3805ek_configure_seq[i].data;
+        if (sci18is606_spi_tx(buf, 3, true) != I2C_STATUS_SUCCESS) {
+            return false;
+        }
+        // Wait for the sensor to restart after the software reset cmd
+        wait_ms(1);
+
+        // Check the sensor ID to validate the spi link after the reset
+        if (i == 1 && buf[1] != PAW3805EK_ID) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
