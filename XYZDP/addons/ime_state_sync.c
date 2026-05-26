3d12ce5d4ce9b82da91d5f76b7a2052574f1e850
kokeko-kokeko <kokeko@mac.com>
2026-05-26 11:53:46 +0900

    Update ime_state_sync.c

--- XYZDP/addons/ime_state_sync.c
+++ XYZDP/addons/ime_state_sync.c
@@ -151,7 +151,8 @@ void housekeeping_task_ime_state_sync(void) {
 
 void set_layer_color_overlay_ime_state_sync(void) {
   // if mouse on exit
-  if (layer_state_is(LAYER_Mouse)) return;
+  if (layer_state_is(LAYER_Mouse_L)) return;
+  if (layer_state_is(LAYER_Mouse_R)) return;
   
   // IME state sync syntem state
   if (ime_on) {
