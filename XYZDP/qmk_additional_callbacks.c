a57c4a49a5cc1c87ba66b921d606d192484c29eb
kokeko-kokeko <kokeko@mac.com>
2026-06-02 15:24:18 +0900

    cleanup

--- XYZDP/qmk_additional_callbacks.c
+++ XYZDP/qmk_additional_callbacks.c
@@ -123,7 +123,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
       status_led(0b0100, led_pattern_off);
       break;
     case LAYER_Number:
-    case LAYER_Cursor:
+    case LAYER_Cursor: 
     case LAYER_Function:   
 	  // mouse on keep top bit
       status_led(0b0011, led_pattern_off);
