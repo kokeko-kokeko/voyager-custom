a774da59285ae8db5db244780aaf0e222abca214
koke <kokeko@mac.com>
2026-06-13 01:20:18 +0900

    remove obsolete

--- XYZDP/obsolete/adv_trackpad.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define AUTO_MOUSE_TIME_TRACKPAD 751
-
-void keyboard_post_init_adv_trackpad(void);
-void post_process_record_adv_trackpad(uint16_t keycode, keyrecord_t *record);
-void housekeeping_task_adv_trackpad(void);
--- XYZDP/obsolete/i18n.h
+++
@@ -1,3 +0,0 @@
-#define JP_RBRC KC_NUHS
-#define JP_RCBR S(KC_NUHS)
-#define JP_RPRN S(KC_9)
