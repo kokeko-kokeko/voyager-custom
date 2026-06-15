7327640072e003a95f96dda82fc1aa1c9fa6cb60
kokeko-kokeko <kokeko@mac.com>
2026-06-15 15:26:52 +0900

    update build script

--- build.sh
+++ build.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-echo "    ---- fetch user side code ---- "
+echo -e "\n\t---- fetch user side code ---- "
 git fetch --all
 
-echo "    ---- update qmk firmware ---- "
+echo -e "\n\t---- update qmk firmware ---- "
 pushd qmk_firmware
 git fetch --all
 git pull
 git submodule update --init --recursive
 popd
 
-echo "    ---- update zsa qmk modules ---- "
+echo -e "\n\t---- update zsa qmk modules ---- "
 pushd qmk_firmware/modules/zsa
 git fetch --all
 git pull
 popd
 
-echo "    ---- sync file ---- "
+echo -e "\n\t---- sync file ---- "
 rsync --archive --checksum --delete --open-noatime --verbose XYZDP/ qmk_firmware/keyboards/zsa/voyager/keymaps/XYZDP/
 
-echo "    ---- git status ---- "
+echo -e "\n\t---- git status ---- "
 git status
 
-echo "    ---- build! ---- "
+echo -e "\n\t---- build! ---- "
 pushd qmk_firmware
 #make zsa/voyager:XYZDP
 qmk compile -kb zsa/voyager -km XYZDP
