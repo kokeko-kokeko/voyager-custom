ec796f54d60ff12df679bf4ab93e2b5d0896558c
wsl_user <you@example.com>
2026-07-02 18:37:22 +0900

    update build

--- build.sh
+++ build.sh
@@ -1,57 +1,44 @@
 #!/bin/bash
 
 echo -e "\n\t---- update zsa qmk modules ---- "
-pushd qmk_firmware
-pushd modules/zsa
-git fetch --all
+pushd qmk_firmware > /dev/null
+pushd modules/zsa > /dev/null
 git pull
 
 echo -e "\n\t---- check zsa qmk modules upstream update ---- "
+git fetch --all
 git --no-pager diff --stat origin/main..upstream/main -- ':(exclude)automouse/automouse.c' 
 
-#echo -e "\n\t---- commit zsa qmk modules to qmk firmware ---- "
-#git add modules/zsa
-#git commit -m "✨(qmk modules): Update modules" || echo "No Modules change"
-#git push
-
-popd
+popd > /dev/null
 
 echo -e "\n\t---- update qmk firmware ---- "
-git fetch --all
 git pull
 git submodule update --init --recursive
 
 echo -e "\n\t---- check qmk firmware upstream update ---- "
+git fetch --all
 git --no-pager diff --stat origin/firmware25..upstream/firmware25 -- ':(exclude).gitmodules' ':(exclude)modules/zsa/'
 
-popd
-
-#echo -e "\n\t---- commit qmk firmware to voyager-custom ---- "
-#git add qmk_firmware
-#git commit -m "✨(qmk): Update firmware" || echo "No QMK change"
-#git push
-
-echo -e "\n\t---- fetch user side code ---- "
-git fetch --all
-
-echo -e "\n\t---- check remote ---- "
-git --no-pager diff --stat HEAD..origin/main
+popd > /dev/null
 
 echo -e "\n\t---- sync file to qmk_firmware keyboards folder ---- "
 rsync --archive --checksum --delete --open-noatime --verbose XYZDP/ qmk_firmware/keyboards/zsa/voyager/keymaps/XYZDP/
-#cp -r XYZDP/ qmk_firmware/keyboards/zsa/voyager/keymaps/XYZDP/
+
+echo -e "\n\t---- fetch user side code, check remote ---- "
+git fetch --all
+git --no-pager diff --stat HEAD..origin/main
 
 echo -e "\n\t---- current git status ---- "
 git --no-pager status
 
 echo -e "\n\t---- build! ---- "
-pushd qmk_firmware
+pushd qmk_firmware > /dev/null
 #make zsa/voyager:XYZDP
 qmk compile -kb zsa/voyager -km XYZDP
 
 #rm -r keyboards/zsa/voyager/keymaps/XYZDP/
 
-popd
+popd > /dev/null
 
 echo -e "\n\t---- done! ---- \a"
 
