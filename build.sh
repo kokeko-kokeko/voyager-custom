5ba1de5e50d6abc28e150315d5c71a3eb438e927
wsl_user <you@example.com>
2026-07-02 14:02:57 +0900

    more derail message

--- build.sh
+++ build.sh
@@ -2,19 +2,26 @@
 
 echo -e "\n\t---- fetch user side code ---- "
 git fetch --all
+
+echo -e "\n\t---- check remote ---- "
 git --no-pager diff --stat HEAD..origin/main
+
 
 echo -e "\n\t---- update qmk firmware ---- "
 pushd qmk_firmware
 git fetch --all
 git pull
 git submodule update --init --recursive
+
+echo -e "\n\t---- check qmk firmware upstream update ---- "
 git --no-pager diff --stat origin/firmware25..upstream/firmware25 -- ':(exclude).gitmodules' ':(exclude)modules/zsa/'
 
 echo -e "\n\t---- update zsa qmk modules ---- "
 pushd modules/zsa
 git fetch --all
 git pull
+
+echo -e "\n\t---- check zsa qmk modules upstream update ---- "
 git --no-pager diff --stat origin/main..upstream/main -- ':(exclude)automouse/automouse.c' 
 
 popd
@@ -31,11 +38,11 @@ popd
 #git commit -m "✨(qmk): Update firmware" || echo "No QMK change"
 #git push
 
-echo -e "\n\t---- sync file ---- "
+echo -e "\n\t---- sync file to qmk firmware ---- "
 rsync --archive --checksum --delete --open-noatime --verbose XYZDP/ qmk_firmware/keyboards/zsa/voyager/keymaps/XYZDP/
 #cp -r XYZDP/ qmk_firmware/keyboards/zsa/voyager/keymaps/XYZDP/
 
-echo -e "\n\t---- git status ---- "
+echo -e "\n\t---- current git status ---- "
 git --no-pager status
 
 echo -e "\n\t---- build! ---- "
