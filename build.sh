346cdefa639c839ec393f22bb1499e6656b32ce9
wsl_user <you@example.com>
2026-07-02 18:19:19 +0900

    update build script

--- build.sh
+++ build.sh
@@ -1,30 +1,13 @@
 #!/bin/bash
 
-echo -e "\n\t---- fetch user side code ---- "
-git fetch --all
-
-echo -e "\n\t---- check remote ---- "
-git --no-pager diff --stat HEAD..origin/main
-
-
-echo -e "\n\t---- update qmk firmware ---- "
-pushd qmk_firmware
-git fetch --all
-git pull
-git submodule update --init --recursive
-
-echo -e "\n\t---- check qmk firmware upstream update ---- "
-git --no-pager diff --stat origin/firmware25..upstream/firmware25 -- ':(exclude).gitmodules' ':(exclude)modules/zsa/'
-
 echo -e "\n\t---- update zsa qmk modules ---- "
+pushd qmk_firmware
 pushd modules/zsa
 git fetch --all
 git pull
 
 echo -e "\n\t---- check zsa qmk modules upstream update ---- "
 git --no-pager diff --stat origin/main..upstream/main -- ':(exclude)automouse/automouse.c' 
-
-popd
 
 #echo -e "\n\t---- commit zsa qmk modules to qmk firmware ---- "
 #git add modules/zsa
@@ -33,10 +16,26 @@ popd
 
 popd
 
+echo -e "\n\t---- update qmk firmware ---- "
+git fetch --all
+git pull
+git submodule update --init --recursive
+
+echo -e "\n\t---- check qmk firmware upstream update ---- "
+git --no-pager diff --stat origin/firmware25..upstream/firmware25 -- ':(exclude).gitmodules' ':(exclude)modules/zsa/'
+
+popd
+
 #echo -e "\n\t---- commit qmk firmware to voyager-custom ---- "
 #git add qmk_firmware
 #git commit -m "✨(qmk): Update firmware" || echo "No QMK change"
 #git push
+
+echo -e "\n\t---- fetch user side code ---- "
+git fetch --all
+
+echo -e "\n\t---- check remote ---- "
+git --no-pager diff --stat HEAD..origin/main
 
 echo -e "\n\t---- sync file to qmk_firmware keyboards folder ---- "
 rsync --archive --checksum --delete --open-noatime --verbose XYZDP/ qmk_firmware/keyboards/zsa/voyager/keymaps/XYZDP/
