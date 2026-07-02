15f0d0ef61149b2aeb8ee59a6edb97d0d36ee73c
wsl_user <you@example.com>
2026-07-03 02:54:26 +0900

    add build massage

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-echo -e "\e[;32m---- fetch only user side code repos ----\e[m"
+echo -e "\e[;32m---- fetch only user side code repo ----\e[m"
 git fetch --all
 
-echo -e "\e[;32m---- update qmk firmware repos ----\e[m"
+echo -e "\e[;32m---- update qmk firmware repo ----\e[m"
 pushd qmk_firmware > /dev/null
 git fetch --all
 git pull
 git submodule update --init --recursive
 popd > /dev/null
 
-echo -e "\e[;32m---- update zsa modules repos ----\e[m"
+echo -e "\e[;32m---- update zsa modules repo ----\e[m"
 pushd qmk_firmware/modules/zsa > /dev/null
 git fetch --all
 git pull
@@ -25,14 +25,14 @@ pushd qmk_firmware > /dev/null
 qmk compile -kb zsa/voyager -km XYZDP
 popd > /dev/null
 
-echo -e "\e[;32m---- check zsa modules upstream update ----\e[m"
-pushd qmk_firmware/modules/zsa > /dev/null
-git --no-pager diff --stat HEAD..upstream/main -- ':(exclude)automouse/automouse.c'
-popd > /dev/null
-
 echo -e "\e[;32m---- check qmk firmware upstream update ----\e[m"
 pushd qmk_firmware > /dev/null
 git --no-pager diff --stat HEAD..upstream/firmware25 -- ':(exclude).gitmodules' ':(exclude)modules/zsa/'
+popd > /dev/null
+
+echo -e "\e[;32m---- check zsa modules upstream update ----\e[m"
+pushd qmk_firmware/modules/zsa > /dev/null
+git --no-pager diff --stat HEAD..upstream/main -- ':(exclude)automouse/automouse.c'
 popd > /dev/null
 
 echo -e "\e[;32m---- check user side code remote update ----\e[m"
