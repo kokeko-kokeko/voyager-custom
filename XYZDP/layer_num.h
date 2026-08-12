f40a9cca2825b956fcc51ee09fe03bedafec857a
wsl_user <you@example.com>
2026-08-12 17:37:07 +0900

    update layer

--- XYZDP/layer_num.h
+++ XYZDP/layer_num.h
@@ -24,15 +24,15 @@ enum layer_num {
   LAYER_R_pinky_1,
 
   LAYER_L_thumb_0_L_pinky_0,
-  LAYER_L_thumb_0_L_pinky_1,
-
   LAYER_R_thumb_0_R_pinky_0,
+
+  LAYER_L_thumb_0_L_pinky_1,
   LAYER_R_thumb_0_R_pinky_1,
  
   LAYER_L_thumb_0_R_pinky_0,
-  LAYER_L_thumb_0_R_pinky_1,
-
   LAYER_R_thumb_0_L_pinky_0,
+
+  LAYER_L_thumb_0_R_pinky_1,
   LAYER_R_thumb_0_L_pinky_1,
 
   LAYER_L_thumb_0_R_thumb_0,
--- XYZDP/qmk_additional_callbacks.c
+++ XYZDP/qmk_additional_callbacks.c
@@ -90,16 +90,16 @@ uint16_t get_flow_tap_term(uint16_t keycode, keyrecord_t* record,
 layer_state_t layer_state_set_user(layer_state_t state) {
   // same side thumb and pin
   state = update_tri_layer_state(state, LAYER_L_thumb_0, LAYER_L_pinky_0, LAYER_L_thumb_0_L_pinky_0);
-  state = update_tri_layer_state(state, LAYER_L_thumb_0, LAYER_L_pinky_1, LAYER_L_thumb_0_L_pinky_1);
-
   state = update_tri_layer_state(state, LAYER_R_thumb_0, LAYER_R_pinky_0, LAYER_R_thumb_0_R_pinky_0);
+
+  state = update_tri_layer_state(state, LAYER_L_thumb_0, LAYER_L_pinky_1, LAYER_L_thumb_0_L_pinky_1);
   state = update_tri_layer_state(state, LAYER_R_thumb_0, LAYER_R_pinky_1, LAYER_R_thumb_0_R_pinky_1);
   
   // cross side thumb and pin
   state = update_tri_layer_state(state, LAYER_L_thumb_0, LAYER_R_pinky_0, LAYER_L_thumb_0_R_pinky_0);
-  state = update_tri_layer_state(state, LAYER_L_thumb_0, LAYER_R_pinky_1, LAYER_L_thumb_0_R_pinky_1);
-
   state = update_tri_layer_state(state, LAYER_R_thumb_0, LAYER_L_pinky_0, LAYER_R_thumb_0_L_pinky_0);
+
+  state = update_tri_layer_state(state, LAYER_L_thumb_0, LAYER_R_pinky_1, LAYER_L_thumb_0_R_pinky_1);
   state = update_tri_layer_state(state, LAYER_R_thumb_0, LAYER_L_pinky_1, LAYER_R_thumb_0_L_pinky_1);
 
   // both thumb
