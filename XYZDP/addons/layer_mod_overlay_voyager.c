b0d781e4871eadcbba2737454139889fe156bb37
wsl_user <you@example.com>
2026-08-24 18:14:12 +0900

    cleanup overlay

--- XYZDP/addons/layer_mod_overlay_voyager.c
+++ XYZDP/addons/layer_mod_overlay_voyager.c
@@ -59,114 +59,6 @@ static void rgb_matrix_indicators_layer_overlay(void) {
     rgb_matrix_set_color(47, 0, 0, 0);
     rgb_matrix_set_color(48, 0, 0, 0);
   }
-  
-  // use golden angle 255 * phi
-  //const uint8_t h_diff = 97;
-  
-  //HSV hsv = rgb_matrix_get_hsv();
-  //hsv.h -= g_rgb_timer >> 4;
-  //hsv.s = 255;
-  
-  // copy logic from breathing_anim.h 
-  // use different speed divide
-  //uint8_t speed = rgb_matrix_get_speed(); 
-  //uint16_t time = scale16by8(g_rgb_timer, speed / 3);
-  //hsv.v = scale8(abs8(sin8(time) - 128) * 2, hsv.v);
-  
-  // layer display with priority
-  //if (layer_state_is(L_BothPinkyThumb)) {
-  //  hsv.h -= h_diff;
-  //  RGB rgb = hsv_to_rgb(hsv);
-  //  rgb_matrix_set_color(5, rgb.r, rgb.g, rgb.b);
-  //  rgb_matrix_set_color(6, rgb.r, rgb.g, rgb.b);
-
-  //  rgb_matrix_set_color(26, rgb.r, rgb.g, rgb.b);
-  //  rgb_matrix_set_color(37, rgb.r, rgb.g, rgb.b);
-
-  //  rgb_matrix_set_color(11, rgb.r, rgb.g, rgb.b);
-  //  rgb_matrix_set_color(22, rgb.r, rgb.g, rgb.b);
-  //  rgb_matrix_set_color(50, rgb.r, rgb.g, rgb.b);
-
-  //  rgb_matrix_set_color(25, rgb.r, rgb.g, rgb.b);
-  //  rgb_matrix_set_color(32, rgb.r, rgb.g, rgb.b);
-  //  rgb_matrix_set_color(45, rgb.r, rgb.g, rgb.b);
-  //  return;
-  //}
-  //if (layer_state_is(L_BothPinky)) {
-  //  hsv.h -= h_diff;
-  //  RGB rgb = hsv_to_rgb(hsv);
-  //  rgb_matrix_set_color(5, rgb.r, rgb.g, rgb.b);
-  //  rgb_matrix_set_color(6, rgb.r, rgb.g, rgb.b);
-
-  //  rgb_matrix_set_color(26, rgb.r, rgb.g, rgb.b);
-  //  rgb_matrix_set_color(37, rgb.r, rgb.g, rgb.b);
-  //  return;
-  //}
-  //if (layer_state_is(L_BothThumb)) {
-  //  hsv.h -= h_diff;
-  //  RGB rgb = hsv_to_rgb(hsv);
-  //  rgb_matrix_set_color(11, rgb.r, rgb.g, rgb.b);
-  //  rgb_matrix_set_color(22, rgb.r, rgb.g, rgb.b);
-  //  rgb_matrix_set_color(50, rgb.r, rgb.g, rgb.b);
-
-  //  rgb_matrix_set_color(25, rgb.r, rgb.g, rgb.b);
-  //  rgb_matrix_set_color(32, rgb.r, rgb.g, rgb.b);
-  //  rgb_matrix_set_color(45, rgb.r, rgb.g, rgb.b);
-  //  return;
-  //}
-  //if (layer_state_is(L_RightPinkyThumb)) {
-  //  hsv.h -= h_diff;
-  //  RGB rgb = hsv_to_rgb(hsv);
-  //  rgb_matrix_set_color(26, rgb.r, rgb.g, rgb.b);
-  //  rgb_matrix_set_color(37, rgb.r, rgb.g, rgb.b);
-    
-  //  rgb_matrix_set_color(25, rgb.r, rgb.g, rgb.b);
-  //  rgb_matrix_set_color(32, rgb.r, rgb.g, rgb.b);
-  //  rgb_matrix_set_color(45, rgb.r, rgb.g, rgb.b);
-
-  //  return;
-  //}
-  //if (layer_state_is(L_LeftPinkyThumb)) {
-  //  hsv.h -= h_diff;
-  //  RGB rgb = hsv_to_rgb(hsv);
-  //  rgb_matrix_set_color(5, rgb.r, rgb.g, rgb.b);
-  //  rgb_matrix_set_color(6, rgb.r, rgb.g, rgb.b);
-
-  //  rgb_matrix_set_color(11, rgb.r, rgb.g, rgb.b);
-  //  rgb_matrix_set_color(22, rgb.r, rgb.g, rgb.b);
-  //  rgb_matrix_set_color(50, rgb.r, rgb.g, rgb.b);
-  //  return;
-  //}
-  //if (layer_state_is(L_RightPinky)) {
-  //  hsv.h -= h_diff;
-  //  RGB rgb = hsv_to_rgb(hsv);
-  //  rgb_matrix_set_color(26, rgb.r, rgb.g, rgb.b);
-  //  rgb_matrix_set_color(37, rgb.r, rgb.g, rgb.b);
-  //  return;
-  //}
-  //if (layer_state_is(L_LeftPinky)) {
-  //  hsv.h -= h_diff;
-  //  RGB rgb = hsv_to_rgb(hsv);
-  //  rgb_matrix_set_color(5, rgb.r, rgb.g, rgb.b);
-  //  rgb_matrix_set_color(6, rgb.r, rgb.g, rgb.b);
-  //  return;
-  //}
-  //if (layer_state_is(L_Cursor)) {
-  //  hsv.h -= h_diff;
-  //  RGB rgb = hsv_to_rgb(hsv);
-  //  rgb_matrix_set_color(25, rgb.r, rgb.g, rgb.b);
-  //  rgb_matrix_set_color(32, rgb.r, rgb.g, rgb.b);
-  //  rgb_matrix_set_color(45, rgb.r, rgb.g, rgb.b);
-  //  return;
-  //}
-  //if (layer_state_is(L_Number)) {
-  //  hsv.h -= h_diff;
-  //  RGB rgb = hsv_to_rgb(hsv);
-  //  rgb_matrix_set_color(11, rgb.r, rgb.g, rgb.b);
-  //  rgb_matrix_set_color(22, rgb.r, rgb.g, rgb.b);
-  //  rgb_matrix_set_color(50, rgb.r, rgb.g, rgb.b);
-  //  return;
-  //}
 }
 
 static void rgb_matrix_indicators_mod_overlay(void) {
@@ -204,132 +96,20 @@ static void rgb_matrix_indicators_mod_overlay(void) {
   }  
   
   if (get_mods() & MOD_BIT_LCTRL) {
-    //rgb_matrix_set_color(10, 0, 0, 0);
-    rgb_matrix_set_color(16, 0, 0, 0);
-
     rgb_matrix_set_color(12, 0, 0, 0);
   }
 
   if (get_mods() & MOD_BIT_LSHIFT) {
-    //rgb_matrix_set_color(9, 0, 0, 0);
-    rgb_matrix_set_color(15, 0, 0, 0);
-
     rgb_matrix_set_color(18, 0, 0, 0);
   }
 
-  if (get_mods() & MOD_BIT_LALT) {
-    //rgb_matrix_set_color(8, 0, 0, 0);
-    rgb_matrix_set_color(14, 0, 0, 0);
-  }
-
-  if (get_mods() & MOD_BIT_LGUI) {
-    //rgb_matrix_set_color(7, 0, 0, 0);
-    rgb_matrix_set_color(13, 0, 0, 0);
-  }  
-
   if (get_mods() & MOD_BIT_RCTRL) {
-    //rgb_matrix_set_color(33, 0, 0, 0);
-    rgb_matrix_set_color(39, 0, 0, 0);
-
     rgb_matrix_set_color(43, 0, 0, 0);
   }
 
   if (get_mods() & MOD_BIT_RSHIFT) {
-    //rgb_matrix_set_color(34, 0, 0, 0);
-    rgb_matrix_set_color(40, 0, 0, 0);
-
     rgb_matrix_set_color(49, 0, 0, 0);
   }
-
-  if (get_mods() & MOD_BIT_RALT) {
-    //rgb_matrix_set_color(35, 0, 0, 0);
-    rgb_matrix_set_color(41, 0, 0, 0);
-  }
-
-  if (get_mods() & MOD_BIT_RGUI) {
-    //rgb_matrix_set_color(36, 0, 0, 0);
-    rgb_matrix_set_color(42, 0, 0, 0);
-  }
-
-  // use golden angle 255 * phi
-  //const uint8_t h_diff = 97;
-  
-  //HSV hsv = rgb_matrix_get_hsv();
-  //hsv.h -= g_rgb_timer >> 5;
-  //hsv.s = 255;
-  
-  // copy logic from breathing_anim.h 
-  // use different speed divide
-  //uint8_t speed = rgb_matrix_get_speed(); 
-  //uint16_t time = scale16by8(g_rgb_timer, speed / 5);
-  //hsv.v = scale8(abs8(sin8(time) - 128) * 2, hsv.v);
-
-  // CAPS WORD inidication
-  //if (is_caps_word_on()) {
-  //  hsv.h -= h_diff;
-  //  RGB rgb = hsv_to_rgb(hsv);
-  //  rgb_matrix_set_color(0, rgb.r, rgb.g, rgb.b);
-  //  rgb_matrix_set_color(31, rgb.r, rgb.g, rgb.b);
-  //}
-
-  // mods display
-  //if (get_mods() & MOD_BIT_LCTRL) {
-  //  hsv.h -= h_diff;
-  //  RGB rgb = hsv_to_rgb(hsv);
-  //  rgb_matrix_set_color(4, rgb.r, rgb.g, rgb.b);
-    //rgb_matrix_set_color(10, rgb.r, rgb.g, rgb.b);
-    //rgb_matrix_set_color(12, rgb.r, rgb.g, rgb.b);
-    //rgb_matrix_set_color(13, rgb.r, rgb.g, rgb.b);
-  //}
-  //if (get_mods() & MOD_BIT_LSHIFT) {
-  //  hsv.h -= h_diff;
-  //  RGB rgb = hsv_to_rgb(hsv);
-  //  rgb_matrix_set_color(3, rgb.r, rgb.g, rgb.b);
-    //rgb_matrix_set_color(9, rgb.r, rgb.g, rgb.b);
-    //rgb_matrix_set_color(14, rgb.r, rgb.g, rgb.b);
-    //rgb_matrix_set_color(18, rgb.r, rgb.g, rgb.b);
-  //}
-  //if (get_mods() & MOD_BIT_LALT) {
-  //  hsv.h -= h_diff;
-  //  RGB rgb = hsv_to_rgb(hsv);
-  //  rgb_matrix_set_color(2, rgb.r, rgb.g, rgb.b);
-    //rgb_matrix_set_color(8, rgb.r, rgb.g, rgb.b);
-  //}
-  //if (get_mods() & MOD_BIT_LGUI) {
-  //  hsv.h -= h_diff;
-  //  RGB rgb = hsv_to_rgb(hsv);
-  //  rgb_matrix_set_color(1, rgb.r, rgb.g, rgb.b);
-    //rgb_matrix_set_color(7, rgb.r, rgb.g, rgb.b);
-  //}
-  
-  //if (get_mods() & MOD_BIT_RCTRL) {
-  //  hsv.h -= h_diff;
-  //  RGB rgb = hsv_to_rgb(hsv);
-  //  rgb_matrix_set_color(27, rgb.r, rgb.g, rgb.b);
-    //rgb_matrix_set_color(33, rgb.r, rgb.g, rgb.b);
-    //rgb_matrix_set_color(42, rgb.r, rgb.g, rgb.b);
-    //rgb_matrix_set_color(43, rgb.r, rgb.g, rgb.b);
-  //}
-  //if (get_mods() & MOD_BIT_RSHIFT) {
-  //  hsv.h -= h_diff;
-  //  RGB rgb = hsv_to_rgb(hsv);
-  //  rgb_matrix_set_color(28, rgb.r, rgb.g, rgb.b);
-    //rgb_matrix_set_color(34, rgb.r, rgb.g, rgb.b);
-    //rgb_matrix_set_color(41, rgb.r, rgb.g, rgb.b);
-    //rgb_matrix_set_color(49, rgb.r, rgb.g, rgb.b);
-  //}
-  //if (get_mods() & MOD_BIT_RALT) {
-  //  hsv.h -= h_diff;
-  //  RGB rgb = hsv_to_rgb(hsv);
-  //  rgb_matrix_set_color(29, rgb.r, rgb.g, rgb.b);
-    //rgb_matrix_set_color(35, rgb.r, rgb.g, rgb.b);
-  //}
-  //if (get_mods() & MOD_BIT_RGUI) {
-  //  hsv.h -= h_diff;
-  //  RGB rgb = hsv_to_rgb(hsv);
-   // rgb_matrix_set_color(30, rgb.r, rgb.g, rgb.b);
-    //rgb_matrix_set_color(36, rgb.r, rgb.g, rgb.b);
-  //}
 }
 
 bool rgb_matrix_indicators_layer_mod_overlay(void) {
