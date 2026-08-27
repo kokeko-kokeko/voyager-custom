38739b6b27c210a67122661fb73396b6da398c82
wsl_user <you@example.com>
2026-08-27 17:27:48 +0900

    impl caps word system

--- XYZDP/addons/layer_caps_word.c
+++ XYZDP/addons/layer_caps_word.c
@@ -10,13 +10,39 @@
 #include "addons/layer_caps_word.h"
 
 // enter layer, no key press, exit run caps word
+static uint8_t caps_layers[] = {LAYER_L_thumb_3, LAYER_R_thumb_3};
 
+#define CAPS_LAYER_COUNT (sizeof(caps_layers) / sizeof(caps_layers[0]))
+
+static bool layer_active[CAPS_LAYER_COUNT] = {false};
+static bool key_pressed[CAPS_LAYER_COUNT] = {false};
 
 bool pre_process_record_layer_caps_word(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        for (int i = 0; i < CAPS_LAYER_COUNT; i++) {
+            key_pressed[i] = true;    
+        }
+    }
+
     return true;
 }
 
 layer_state_t layer_state_set_layer_caps_word(layer_state_t state) {
+    for (int i = 0; i < CAPS_LAYER_COUNT; i++) {
+        if (layer_state_cmp(state, caps_layers[i]) == layer_active[i]) continue;
+        layer_active[i] = !(layer_active[i]);
+
+        if (layer_active[i]) {
+            // enter layer
+            key_pressed[i] = false; 
+        } else {
+            // leave layer
+            if (key_pressed[i] == false) {
+                // non key press, caps word
+                caps_word_toggle();
+            }
+        }
+    }
 
     return state;
 }
