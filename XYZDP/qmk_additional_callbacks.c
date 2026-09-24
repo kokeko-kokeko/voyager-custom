9bb424f73c8c739592a2c01d4b0cbae449c97740
wsl_user <you@example.com>
2026-09-24 20:13:47 +0900

    update term setting

--- XYZDP/qmk_additional_callbacks.c
+++ XYZDP/qmk_additional_callbacks.c
@@ -35,7 +35,8 @@ void chSysIdleHook(void) {
 // per-key tapping parameter
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
   // PFON : Pos Func ONly, dummy in A -> no wait
-  if (keycode == PFON(KC_A)) return 0;
+  if ((IS_QK_MOD_TAP(keycode) == true) && (QK_MOD_TAP_GET_MODS(keycode) == MOD_PFON)) return 0;
+  //if (keycode == PFON(KC_A)) return 0;
 
   return TAPPING_TERM;
 }
