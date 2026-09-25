298cb70357a0ddb6433c9aa5ecebdcd2c9170944
wsl_user <you@example.com>
2026-09-25 12:38:05 +0900

    commit test

--- XYZDP/addon_hooks.c
+++ XYZDP/addon_hooks.c
@@ -97,7 +97,7 @@ bool pre_process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
-// process_record_user in keymap.c, create _addon_hooks and insert it
+// process_record_user in keymap.c, create _addon_hooks and insert to keymap.c
 bool process_record_addon_hooks(uint16_t keycode, keyrecord_t *record) {
   //if (process_record_fade_matrix(keycode, record) == false) return false;
   if (process_record_flexible_behavior_os_locale(keycode, record) == false) return false;
